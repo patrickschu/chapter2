b0391cf0f93f5b9f3e098f329f8a7c3ef9580fca
patrick <ps22344@par102-894475m.local>
2016-07-29 16:45:49 -0500

    finished word2vec with output

--- current/test_word2vec.py
+++ current/test_word2vec.py
@@ -31,8 +31,10 @@ def adtextextractor(text, fili):
 #separate into sentences
 now=time.time()
 pathi=os.path.expanduser(os.path.join("~/", "Downloads", "craigbalanced_0601"))
+
+pathi=os.path.expanduser(os.path.join("~/", "Downloads", "craig_0208"))
 folderlist=[i for i in os.listdir(pathi) if not i.startswith(".")]
-#folderlist=['3']
+
 print "folders", folderlist
 
 
@@ -62,13 +64,11 @@ def sentencefeeder(folder_list):
 			sents=[s for longsent in sents for s in longsent]
 			sents=[s.lower() for s in sents if s and not excluderegex.match(s)]
 			for sent in sents:
-				#print [re.sub(punctuationregex, "assssiAAAA",s) for s in sent.split()]
-				# print [re.sub(excluderegex, "ASSSI", s) for s in sent]
- 				sent=[re.sub(punctuationregex, "",s) for s in sent.split()]
+				sent=[re.sub(punctuationregex, "",s) for s in sent.split()]
  				#yield [stemmer.stem(s) for s in sent if s]
  				yield [s for s in sent if s]
 
-model = Word2Vec(size=1000, min_count=20, workers=4, sg=1)
+model = Word2Vec(size=300, min_count=20, workers=4, sg=1)
 			
 model.build_vocab(sentencefeeder(folderlist))
 model.train(sentencefeeder(folderlist))
@@ -78,54 +78,18 @@ model.save("model_1")
 #look at it
 print "model saved"
 
+newmod=model
+print "sex", newmod.most_similar(positive=['sex'])
+print 'woman', newmod.most_similar(positive=['woman'])
+print 'girl',newmod.most_similar(positive=['girl'])
 
+print 'man',newmod.most_similar(positive=['man'])
+print 'guy',newmod.most_similar(positive=['guy'])
 
+print 'gay',newmod.most_similar(positive=['gay'])
 
 #class gensim.models.word2vec.Word2Vec(sentences=None, size=100, alpha=0.025, window=5, min_count=5, max_vocab_size=None, sample=0.001, seed=1, workers=3, min_alpha=0.0001, sg=0, hs=0, negative=5, cbow_mean=1, hashfxn=<built-in function hash>, iter=5, null_word=0, trim_rule=None, sorted_vocab=1, batch_words=10000
 
-
-
-
-
-# 
-# print np.prod([ 0.09915783, -0.03299134, -0.01437753,  0.00536732,  0.06406488])
-# print np.prod([ 0.08549197, -0.075089,   -0.01246947, -0.04792206, -0.00101304])
-# print np.prod([ 0.03188382,  0.0699484,  -0.0095439,  -0.04693603, -0.07593358])
-# 
-# documents = ["Human machine interface for lab abc computer applications",
-#              "A survey the of the user opinion of computer the system the response time",
-#              "The EPS user interface management system",
-#              "the System the and human system engineering testing of EPS",
-#              "Relation the of the user perceived response time to error measurement",
-#              "The generation the of the random binary unordered trees",
-#              "The intersection graph the of the paths in trees",
-#              "Graph minors IV Widths the of the trees and well quasi ordering",
-#              "Graph minors A survey"]
-# 
-# pprint.pprint(documents)
-# # 
-# docs=[[w.lower() for w in doc.split() if len(w) > 1]for doc in documents]
-# 
-# model = Word2Vec()
-# print model.scan_vocab(docs)#.raw_vocab
-# 
-# model.build_vocab([i for i in docs])
-# print "dict length", len(model.vocab), "\n"
-# for entry in model.vocab:
-# 	print "entry '", entry, "'", model.vocab[entry]
-# print model.syn0
-# model.train(['the','the','the','the','of'])
-# print "dict length", len(model.vocab), "\n"
-# #print model.vocab['trees']
-# for entry in model.vocab:
-# 	print "entry '", entry, "'", model.vocab[entry]
-
-
-#print model.syn0
-#for s in model.syn0:
-#	print len(s)
-
-#model.vocab stores the dictionary of word counts and word indexea
 
 
 # from source code 
@@ -136,44 +100,10 @@ print "model saved"
 #             self.train(sentences)
 
 
-# 
-# newsi=dict.doc2bow("Relation of user perceived response time to error measurement".split())
-# #print newsi
-# 
-# 
-# model = Word2Vec(docs, size=100, window=5, min_count=3, workers=4)
-# print model.most_similar(positive=["relation"])
-#print model['user']
 
-# now=time.time()
-# pathi=os.path.expanduser(os.path.join("~/", "Downloads", "craigbalanced_0601", "1"))
-# 
-# filis=[i for i in os.listdir(os.path.join(pathi)) if not i.startswith(".")]
-# 
-# model= Word2Vec(size=2000)
-# 
-# def getgo(filis):
-# 	for f in filis:
-# 		with open(os.path.join(pathi,f), "r") as inputfile:
-# 		 	yield([i.lower() for i in inputfile.read().split()])
-# 
-# model.build_vocab(getgo([i for i in os.listdir(os.path.join(pathi)) if not i.startswith(".")]))
-# #model.syn0 is the entire dictionary
-# model.save("word2vecmodel")
-# print model.syn0.shape
+
 # 
 # #Note that there is a gensim.models.phrases module
 # 
-# end=time.time()
-# print "this took us freaking {} minutes!".format((end-now)/60)
-# 
-# 
-# featureVec=np.array([[1.,2,3,4],[22.,33,11,0], [1.,2,3,4]])
-# print featureVec
-# featureVec = np.divide(featureVec,2)
-# print featureVec
-
-
-# gensim.models.phrases 
-# 
-# he methods accept an iterable of sentences
+end=time.time()
+print "this took us freaking {} minutes!".format((end-now)/60)
