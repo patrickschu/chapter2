27b8bc3b09ad1fb594f5aaeaaa2792d70ad18a67
patrick <ps22344@par102-894475m.local>
2016-07-29 13:05:02 -0500

    add extraction of words from word2vec model to untitled text 3

--- current/untitled text 3.py
+++ current/untitled text 3.py
@@ -10,28 +10,33 @@ header="\n\n----\n\n"
 newmod=Word2Vec.load("model_1")
 #newmod=Word2Vec.load_word2vec_format('/Users/ps22344/Downloads/GoogleNews-vectors-negative300.bin.gz', binary=True)
 print header, "working with ", newmod.syn0.shape, header
-print newmod.vocab
-print "sex", newmod.most_similar(positive=['sex'])
-print 'woman', newmod.most_similar(positive=['woman'])
-print 'girl',newmod.most_similar(positive=['girl'])
-
-print 'man',newmod.most_similar(positive=['man'])
-print 'guy',newmod.most_similar(positive=['guy'])
-
-print 'gay',newmod.most_similar(positive=['gay'])
+#print newmod.syn0
+for key  in newmod.vocab:
+	print key, newmod.vocab[key].__dict__['index']
 
 
-for k in [2, 4,8,16,32]:
-	#clustering=AgglomerativeClustering(n_clusters=k, affinity='cosine', linkage='complete')
-	clustering=KMeans(n_clusters=k, max_iter=1000, n_init=100)
-	result=clustering.fit(newmod.syn0)
-	clusteringstats=ct.Clusteringstats(newmod.syn0, newmod.syn0, result, result.labels_)
-	print header, k
-	print clustering
-	print clusteringstats.size_of_clusters()
-	print clusteringstats.cluster_silhouette('cosine')
-	#link label to word, group by clusters
-	#matrix_with_cats, matrix_without_cats, name, labels , centroids=None
+# print "sex", newmod.most_similar(positive=['sex'])
+# print 'woman', newmod.most_similar(positive=['woman'])
+# print 'girl',newmod.most_similar(positive=['girl'])
+# 
+# print 'man',newmod.most_similar(positive=['man'])
+# print 'guy',newmod.most_similar(positive=['guy'])
+# 
+# print 'gay',newmod.most_similar(positive=['gay'])
+
+
+# for k in [2, 4,8,16,32]:
+# 	print "clustering"
+# 	#clustering=AgglomerativeClustering(n_clusters=k, affinity='cosine', linkage='complete')
+# 	clustering=KMeans(n_clusters=k, max_iter=1000, n_init=100)
+# 	result=clustering.fit(newmod.syn0)
+# 	clusteringstats=ct.Clusteringstats(newmod.syn0, newmod.syn0, result, result.labels_)
+# 	print header, k
+# 	print clustering
+# 	print clusteringstats.size_of_clusters()
+# 	print clusteringstats.cluster_silhouette('cosine')
+# 	#link label to word, group by clusters
+# 	#matrix_with_cats, matrix_without_cats, name, labels , centroids=None
 
 
 
