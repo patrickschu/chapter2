0e5a528610912431f176132e28c20022493a9351
patrick <ps22344@par102-894475m.local>
2016-07-30 19:13:26 -0500

    make clustertester executable, works

---
+++ current/02_word2vec_clustertester.py
@@ -0,0 +1,31 @@
+#clustertest
+import os
+import json
+import codecs
+
+header="\n\n\n-----\n"
+# dir is the place where our jsons of clusters hang out
+dir = '/Users/ps22344/Downloads/chapter2/current/outputfiles'
+jsons=[i for i in os.listdir(dir) if not i.startswith(".")]
+
+
+
+def clustertester(filelist):
+	#read in json file, print out content of each cluster
+	for fili in filelist:
+		print fili
+		with codecs.open(os.path.join(dir, fili), "r", "utf-8") as jsoninput:
+			datafile=json.load(jsoninput)
+		for entry in datafile:
+			print header, "******", entry, "******"
+			print datafile[entry]['words']
+		print header
+
+
+
+
+clustertester(jsons)
+
+
+
+
