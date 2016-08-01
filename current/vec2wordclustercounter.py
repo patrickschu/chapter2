1d20fe82b3ababa383a387a4f94d6c875dc21c32
patrick <ps22344@par102-894475m.local>
2016-07-31 21:15:52 -0500

    add word count functionality

--- current/vec2wordclustercounter.py
+++ current/vec2wordclustercounter.py
@@ -1,15 +1,27 @@
 #counting clusters
 import os
+import re
 import json
 import codecs
+from collections import defaultdict
+import clustertools as ct
+from nltk import word_tokenize
+from string import punctuation
 
 pathi=os.path.expanduser(os.path.join("~/", "Downloads", "craigbalanced_0601"))
+punctuationregex=re.compile("(["+"|\\".join(list(punctuation))+"|\d+]+)")
 
 
 folders=[i for i in os.listdir(pathi) if not i.startswith(".")]
 print folders
 
-def vec2wordclustercounter(folderlist, threshold, remove_stopwords=True, remove_punct=True):
+#go thru text: we get a list of items
+
+#instead of counting word, add to cluster it occurs in
+
+
+
+def vec2wordclustercounter(folderlist, cluster_dictionary):
 	"""
 	
 	This is stolen from the cluster_analysis dictmaker. 
@@ -18,38 +30,41 @@ def vec2wordclustercounter(folderlist, threshold, remove_stopwords=True, remove_
 	remove_stopwords used the stopword list defined above to ignore words. 
 	remove_punct works with string.punctuation, cf above. 
 	"""
-	#threshold sets how many times a word needs to occur to be included in the featuredict
-	vocab={}
+	with codecs.open(cluster_dictionary, "r", "utf-8") as inputjson:
+		clusterdict=json.load(inputjson)
+	result=defaultdict(int)
+	#this is just for qc
+	misses=[]
 	for folder in folderlist:
 		filis=[i for i in os.listdir(os.path.join(pathi,folder)) if not i.startswith(".")]
 		print "Building vocab: we have {} files in folder {}".format(len(filis), folder)
-		#collect a dictionary with all words
-		#lowercase them    
 		for fili in filis:
 			inputfile=codecs.open(os.path.join(pathi, folder, fili), "r", "utf-8").read()
 			inputtext=ct.adtextextractor(inputfile, fili)
 			#pre-processing here
-			inputtext=adcleaner(inputtext ,replace_linebreak=True, remove_html=False)
+			inputtext=ct.adcleaner(inputtext ,replace_linebreak=True, remove_html=False)
 			splittext=word_tokenize(inputtext)
 			splittextlo=[i.lower() for i in splittext]	
 			finaltext=[punctuationregex.sub("",i) for i in splittextlo]
 			finaltext=[i for i in finaltext if i and i not in ['br']]	
 			#do we want to lemmatize or things like that
 			for word in finaltext:
-				if word not in vocab:
-					vocab[word]=1
+				cluster= [k for k,v in clusterdict.items() if word in v['words']]
+				if len(cluster) > 1:
+					print "Warning: The item {} was found in more than one clusters".format(word)
+				if len(cluster) < 1:
+					#print "Warning: The item could not be found in a cluster"
+					misses.append(word)
 				else:
-					vocab[word]=vocab[word]+1
-	print "Our vocab dictionary has {} entries".format(len(vocab))
+					result[cluster[0]]=result[cluster[0]]+1
+	print "Our vocab dictionary has {} entries".format(len(result))
 	ct.dictwriter(os.path.join("~/", chapterdir[0], "outputfiles", "fulldict_"+time.strftime("%H_%M_%m_%d")), vocab)
-	if remove_stopwords:
-		vocab= {key:value for key, value in vocab.items() if key not in stopwords }
-		print "After stop word removal, dict is {} long".format(len(vocab))
-	if remove_punct:
-		vocab= {key:value for key, value in vocab.items() if key not in punctuation }
-		print "After punctuation removal, dict is {} long".format(len(vocab))
-	featuredict= {key:value for key, value in vocab.items() if value > float(threshold) }
-	print "Our feature dictionary has {} entries\n---------------\n".format(len(featuredict))
-	print "This is our featuredict", featuredict
-	ct.dictwriter(os.path.join("~/", chapterdir[0], "outputfiles", "featuredict_"+time.strftime("%H_%M_%m_%d")), featuredict)
+# 	featuredict= {key:value for key, value in vocab.items() if value > float(threshold) }
+# 	print "Our feature dictionary has {} entries\n---------------\n".format(len(featuredict))
+# 	print "This is our featuredict", featuredict
+# 	ct.dictwriter(os.path.join("~/", chapterdir[0], "outputfiles", "featuredict_"+time.strftime("%H_%M_%m_%d")), featuredict)
+	print "misses", len(misses)
+	print result
 	return featuredict
+	
+vec2wordclustercounter(folders, '/Users/ps22344/Downloads/chapter2/current/clusters_74_19_45_07_31.json')
