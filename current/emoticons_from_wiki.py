6efac505ea9dec0b46b51dba48454f4eae60d1b3
patrick <ps22344@dhcp-128-83-125-120.dwrl.utexas.edu>
2016-09-10 12:03:02 -0500

    script to extract emoticons from Wikipedia page

---
+++ current/emoticons_from_wiki.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+#read the emoticons from Wikipedia page
+
+import codecs
+from bs4 import BeautifulSoup
+
+
+with codecs.open("/Users/ps22344/Desktop/List of emoticons - Wikipedia, the free encyclopedia.htm", "r") as inputi:
+	wikitext=inputi.read()
+	
+soup=BeautifulSoup(wikitext, "html.parser")
+
+results=[]
+
+for i in soup.find_all('td'):
+	if not i.attrs:
+		print i
+		results.append(i.text)
+	
+#print results
+outski=codecs.open("outski.txt", "a", "utf-8") 
+output=[]
+for r in results:
+	spliti=r.split()
+	for s in spliti:
+		print "\n\n", s, len(s)
+		judgment=raw_input("Y es or N o     ")
+		output.append([s, judgment])
+		outski.write("\t".join([s, judgment])+"\n")
+
+outski.close()
+print output
+
+
+emolist=codecs.open("emolist.txt", "a", "utf-8") 
+for emo,judg in output:
+	if judg == "Y":
+		emolist.write(emo+"\n")
+
+emolist.close()
+
+	
+
+
+
