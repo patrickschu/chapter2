cec86fbc0aa06b07947188c735844018c05f4c52
patrick <ps22344@dhcp-128-83-125-120.dwrl.utexas.edu>
2016-09-13 14:09:43 -0500

    add plotting of percentage difference

--- current/categories_over_clusters_0913.R
+++ current/categories_over_clusters_0913.R
@@ -1,6 +1,8 @@
 ## plots the percentage of categories contained in each cluster
 ## relates them to 'expected', random assignment of data points
 #data is based on table on pg 25 in chapter 2
+#in the future, 'seq_along' might be a good R funcion to use
+
 #last entry is expected value
 cluster0=c(62, 29, 62, 55, 52)
 cluster1=c(26, 12, 26, 23, 22)
@@ -8,26 +10,37 @@ cluster2=c(11, 27, 12, 19, 17)
 cluster3=c(1, 31, 0, 3, 9)
 
 clustering=data.frame(cluster0, cluster1, cluster2, cluster3, row.names=c("m4w", "m4m", "w4m", "w4w", "mean"))
-
+plot(c(100, 200), ylim=c(-100, 100), xlim=c(0, length(colnames(clustering))-1), type="n");
+text(x=1, y=50, "XXX")
 
 clusterplotter = function(dataframe)
 #this takes a dataframe as input
 #needs to be formatted colnames = clustering, categories and mean as row names
-
-
 {
+#we need to subtract 1 because of Python counting
+no_of_clusters=c(0:(length(colnames(dataframe))-1));
+print (no_of_clusters);
+count=0;	
+plot(c(100, 200), ylim=c(-100, 100), xlim=c(0, length(colnames(dataframe))-1), type="n");
 
-print ("watch this");
-print (dataframe['mean',]);
+
 for (c in colnames(dataframe))
-{
+	{
+	cat ('c: ', c, "\n");
+	cat ('count', count, "\n");
+	cat ('cluster: ', no_of_clusters[count], "\n");
+	subseti=data.frame(dataframe[[c]]);
+	rownames(subseti) = rownames(dataframe);
+	#add label for expected value
+	text (x=count, y=0, as.character(subseti['mean',]));	
+	#compute percentages
+	cat ("subseti");
+	print (subseti);
+	print((subseti-subseti['mean',])/(subseti['mean',]/100));
 	
-print (c);
-subseti=data.frame(dataframe[[c]]);
-rownames(subseti) = rownames(dataframe);
-print (subseti);
-
-}
+	#text (c(count, 0), as.character(c));
+	count=count+1
+	}
 	
 }
 
