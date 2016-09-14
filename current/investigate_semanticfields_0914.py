31918e42381690b20e6659628c6514ae7f3416a9
patrick <patrickschultz@utexas.edu>
2016-09-14 10:18:11 -0500

    this script counts the occurrence of individual words within a list of word2vec clusters supplied

---
+++ current/investigate_semanticfields_0914.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+#the script counts the occurence of specific words within certain clusters from word2vec.
+#these clusters are contained in /Users/ps22344/Downloads/chapter2/current/clusterskmeans_54_19_10_07_30.json
+#we need to look at clusters [50, 6, 22, 39, 24, 48, 1]
+#for each cluster, we need a dict that counts occureence of a word in the files in /Users/ps22344/Downloads/craig_0208
+#if needed, these should be broken down by category
