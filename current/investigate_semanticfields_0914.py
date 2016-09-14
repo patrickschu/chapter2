a0021c5017c6e0293e342804766cf812e4f0e2a3
patrick <patrickschultz@utexas.edu>
2016-09-14 11:28:32 -0500

    add folderreader for easier file reading

--- current/investigate_semanticfields_0914.py
+++ current/investigate_semanticfields_0914.py
@@ -1,8 +1,43 @@
 # -*- coding: utf-8 -*-
 
-
+import codecs
+import json
+import os
+from collections import defaultdict
 #the script counts the occurence of specific words within certain clusters from word2vec.
 #these clusters are contained in /Users/ps22344/Downloads/chapter2/current/clusterskmeans_54_19_10_07_30.json
 #we need to look at clusters [50, 6, 22, 39, 24, 48, 1]
 #for each cluster, we need a dict that counts occureence of a word in the files in /Users/ps22344/Downloads/craig_0208
 #if needed, these should be broken down by category
+
+def folderreader(directory):
+	"""
+	folderreader takes a directory containing subfolders. 
+	then iterates over files contained in the subfolder.
+	"""
+	output=[]
+	directory=os.path.expanduser(directory)
+	subfolds=[i for i in os.listdir(directory) if not i.startswith (".")]
+	for fold in subfolds:
+		filis=[os.path.join(directory, fold, i) for i in os.listdir(os.path.join(directory, fold)) if not i.startswith (".")]
+		output=output+filis
+		if len(output) != len (set(output)):
+			print "Alarm \n --- duplicates in filelist --- \n"
+	return (set(output))
+	
+#folderreader('~/Downloads/craig_0208')
+
+def wordcounter (list_of_clusters):
+	with codecs.open('/Users/ps22344/Downloads/chapter2/current/clusterskmeans_54_19_10_07_30.json', 'r', 'utf-8') as jsoninput:
+		wordtovecclusters=json.load(jsoninput)
+	wordtovecclusters={int(k):v['words'] for k,v in wordtovecclusters.items() if int(k) in list_of_clusters}
+	for key in wordtovecclusters:
+		wordcount=defaultdict(float)
+		filis=folderreader('~/Downloads/craig_0208')
+		print "we have {} files to work with".format(len(filis))
+		
+		
+		#[ for word in wordtovecclusters[key]]
+		
+	
+wordcounter([1,2,3])
