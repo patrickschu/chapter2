37f29dcb5a4e573748403025976abad6abac2bbb
patrick <patrickschultz@utexas.edu>
2016-09-27 13:08:10 -0500

    finish emoticon extraction tool

--- current/emoticoncounter.py
+++ current/emoticoncounter.py
@@ -3,10 +3,10 @@ import nltk
 import codecs
 import os
 import clustertools as ct
+import time
 
-
+#create the featuredict from a text file
 featuredict={}
-
 with codecs.open('/Users/ps22344/Downloads/chapter2/textfiles/emolist_final.txt', "r", "utf-8") as inputtext:
 	for line in inputtext.readlines():
 		featuredict[line.rstrip("\n")]=0
@@ -14,29 +14,35 @@ with codecs.open('/Users/ps22344/Downloads/chapter2/textfiles/emolist_final.txt'
 
 
 
-
+#test formatting
 for k in featuredict:
 	if k.startswith(" "):
 		print k
 
 
 
+#dataset
+dir='/Users/ps22344/Downloads/craig_0208/'#adfiles_output_0116'
 
-def tokenizer(input_string):
-	"""
-	The tokenizer takes a string of words.
-	It fixes quirky punctuation that trips us the NLTK Word Tokenizer. 
-	Then it runs nltk.word_tokenize() to return a list of words>
-	"""
-	addspace=stopregex.sub(r"\g<1> \g<2>", input_string)
-	splittext=nltk.word_tokenize(addspace)
-	return splittext
-	
-	
-pati='/Users/ps22344/Downloads/craig_0208/adfiles_output_0116'
-for fili in [i for i in os.listdir(pati) if not i.startswith(".")]:
-	fili=codecs.open(os.path.join(pati, fili), "r", "utf-8")
-	inputad=ct.adtextextractor(fili.read(), fili)
+
+#check if we find items
+starttime=time.time()
+
+for pati in [i for i in os.listdir(dir) if not i.startswith(".")]:
+	print pati
+	for fili in [i for i in os.listdir(os.path.join(dir, pati)) if not i.startswith(".")]:
+		fili=codecs.open(os.path.join(dir, pati, fili), "r", "utf-8")
+		inputad=ct.adtextextractor(fili.read(), fili)
+		words=ct.tokenizer(inputad)
+		for item in words:
+			if item in featuredict:
+				featuredict[item] = featuredict[item]+1
+
+print featuredict
+
+endtime=time.time()
+
+print "This took us {} minutes".format((endtime-starttime)/60)
 	
 
 # inputad="I have been home for days now.what else is     there to do ?! Give me the icecream..."
