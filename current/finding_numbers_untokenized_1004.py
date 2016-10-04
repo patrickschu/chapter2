669eaabc20923273f568985e9cc2fedf1c842704
patrick <patrickschultz@utexas.edu>
2016-10-04 13:44:38 -0500

    this is the script to find all instances of numbers via regex. more flexible and less cool than the tokenfinder, which is based on this

--- current/finding_numbers_untokenized_1004.py
+++ current/finding_numbers_untokenized_1004.py
@@ -30,8 +30,10 @@ for f in numbersdict:
 numbersdict={}
 
 for numba in [0,1,2,3,5,6,7,8,9]:
-	numbersdict[re.compile(r"[a-z]+"+unicode(numba)+"+[a-z]+")]=0
+	numbersdict[re.compile(".{,20}\W+"+unicode(numba)+"\W+.{,20}")]=0
 	
+#numbersdict={re.compile(".*5sunshyne.*"):0}
+
 
 for f in numbersdict:
 	print f.pattern
@@ -53,13 +55,13 @@ for pati in [i for i in os.listdir(dir) if not i.startswith(".")]:
 	the twodict counts words with a distance of 2, the onedict counts words with a distance of 1.
 	"""
 	print pati
-	for fili in [i for i in os.listdir(os.path.join(dir, pati)) if not i.startswith(".")]:
-		fili=codecs.open(os.path.join(dir, pati, fili), "r", "utf-8")
+	for fil in [i for i in os.listdir(os.path.join(dir, pati)) if not i.startswith(".")]:
+		fili=codecs.open(os.path.join(dir, pati, fil), "r", "utf-8")
 		inputad=ct.adtextextractor(fili.read(), fili)
 		inputad=inputad.lower()
 		matches=[k.findall(inputad) for k in numbersdict.keys()]
 		if sum([len(i) for i in matches]) > 0:
-			print "hits", sum([len(i) for i in matches])
+			print "hits", sum([len(i) for i in matches]), fil
 			print matches
 		
 
