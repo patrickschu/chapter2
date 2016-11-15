b47a9a47058d29f09835c4b27e64f50bfae5ad68
patrick <patrickschultz@utexas.edu>
2016-11-15 08:57:27 -0600

    change dictionary creation, add school category

--- current/categorymachine_1114.py
+++ current/categorymachine_1114.py
@@ -2,18 +2,28 @@ import json
 import codecs
 
 
-listi=['aaa', 'bbb', 'acronym', 'trash']
+#note that we can use this to make machine below more flexible
+categories=['location', 'school', 'X']
 
 shorteningdict={
 
-'acronym':{'location':[], 'X':[]},
-'alphabetism': {'location':[], 'X':[]},
-'blend': {'location':[], 'X':[]},
-'abbreviation': {'location':[], 'X':[]},
-'other': {'location':[], 'X':[]},
-'delete':{'location': [], 'X':[]}
+'acronym':{},
+'alphabetism': {},
+'blend': {},
+'abbreviation': {},
+'other': {},
+'delete':{}
 
 }
+
+
+for c in categories:
+	for entry in shorteningdict:
+		shorteningdict[entry][c] = []
+
+print shorteningdict
+
+
 
 
 
@@ -25,14 +35,15 @@ def categorymachine(json_in, outputname):
 	This is used to put the acronyms ID with the acronymfinder in their place.  
 	"""
 	leftoverlist=[]
-	#read in json
 	with codecs.open(json_in, "r", "utf-8") as jsoninput:
 		inputdict=json.load(jsoninput)
 	print "Dict is {} long".format(len(inputdict))
+	
+	
 	#iterate over keys
-	for item in inputdict.keys():
-		print item, "\n", len(item)
-		ini=raw_input("\nALphabetism, ACronym, ABbreviation, Blend, Other, or Delete? ")
+	for item in [i for i in sorted(inputdict, key=inputdict.get, reverse=True) if inputdict[i] > 5]:
+		print "\n", item, "\n", len(item)
+		ini=raw_input("ALphabetism, ACronym, ABbreviation, Blend, Other, or Delete? ")
 		match=[i for i in shorteningdict.keys() if i.startswith(ini.lower())]
 		if len(match) != 1:
 			print "WARNING NO OR TOO MANY MATCHES IN DICT KEYS: ", len(match)
@@ -43,12 +54,17 @@ def categorymachine(json_in, outputname):
 				shorteningdict[match[0]]['location'].append(item)
 			else:
 				shorteningdict[match[0]]['X'].append(item)
+			in_3=raw_input("School or Not? ")
+			if ini_2 in ["S", "s"]:
+				shorteningdict[match[0]]['school'].append(item)
+			else:
+				shorteningdict[match[0]]['X'].append(item)
 	#we need to make sure theyre in the same order as the printout
 	#the json does not come in sorted i think so that needs to happen after
 	print shorteningdict
 	print "leftovers:\n", leftoverlist
-	with codecs.open("shorteningdict_1114.json", "w", "utf-8") as outputi:
+	with codecs.open("shorteningdict_"+outputname+"_1115.json", "w", "utf-8") as outputi:
 		json.dump(shorteningdict, outputi)
 
 
-categorymachine('/Users/ps22344/Downloads/chapter2/current/output_acronyms6letters.json', "X")	
+categorymachine('/Users/ps22344/Downloads/chapter2/current/output_acronyms2letters.json', "6")	
