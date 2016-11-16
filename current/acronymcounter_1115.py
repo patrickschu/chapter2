df540c78cd5c24e9a946f56726fe234e49468c54
patrick <patrickschultz@utexas.edu>
2016-11-16 09:23:55 -0600

    add exception list for faulty plurals

--- current/acronymcounter_1115.py
+++ current/acronymcounter_1115.py
@@ -9,14 +9,14 @@ from collections import defaultdict
 
 
 filelist=[
-"/Users/ps22344/Downloads/chapter2/current/shorteningdict_2_1115.json",
+#"/Users/ps22344/Downloads/chapter2/current/shorteningdict_2_1115.json",
 #"/Users/ps22344/Downloads/chapter2/current/shorteningdict_3_1115.json",
-#"/Users/ps22344/Downloads/chapter2/current/shorteningdict_4_1115.json",
+"/Users/ps22344/Downloads/chapter2/current/shorteningdict_4_1115.json",
 #"/Users/ps22344/Downloads/chapter2/current/shorteningdict_5_1115.json",
 #"/Users/ps22344/Downloads/chapter2/current/shorteningdict_6_1115.json"
 ]
 
-search_terms = ["MILF"]
+search_terms = []
 
 for fili in filelist:
 	with codecs.open(fili, "r", "utf-8") as inputfile:
@@ -45,21 +45,26 @@ def acronymcounter(acronym_list, input_dir):
 		e.g.
 		acronym_list=['LOL', 'ROFL', 'ASL', 'BRB']
 		result=[0,0,2,0] 
+		NOTE:we can consider running location and schools over a different regex that does not include plural s.
 		"""
+		excludelist=set(["oks", "fbs", "PSS", "VAS", "vas", "BCS", "bcs", "NES", "nes", "SMS", "sms", "SAS", "SSS", "sss", "nsas", "mias"])
+		
+		#dicts to store results
 		dicti=defaultdict(float)
 		matchesdicti=defaultdict(list)
+		
 		#regex, lower and pluralize
 		acronym_list=[re.compile("\W((?:"+i+"|"+i.lower()+")[sS]?)\W") for i in acronym_list]
 		acronym_list=set(acronym_list)
 		print [i.pattern for i in acronym_list]
+		#iterate and match
 		for dir in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
 			print dir
 			for fili in [i for i in os.listdir(os.path.join(input_dir, dir)) if not i.startswith(".")]:
-				#print os.path.join(input_dir, dir, fili)
 				with codecs.open(os.path.join(input_dir, dir, fili), "r", "utf-8") as inputtext:
 					inputad=ct.adtextextractor(inputtext.read(), fili)
 				#result is a list of lists which contain matches for each regex/acronym
-				result=[(i.findall(inputad), i.pattern) for i in acronym_list] 
+				result=[([m for m in i.findall(inputad) if not m in excludelist], i.pattern) for i in acronym_list] 
 				for matches, pattern in result:
  					#the dicti is {pattern:count, pattern: count, ...}
  					dicti[pattern]=dicti[pattern]+len(matches)
