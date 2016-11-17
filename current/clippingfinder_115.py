b1189c2c4890ad49f738a8029c62b751ccfbe9c4
patrick <patrickschultz@utexas.edu>
2016-11-17 14:52:01 -0600

    add comments on procedure

--- current/clippingfinder_115.py
+++ current/clippingfinder_115.py
@@ -9,7 +9,10 @@ import tokenfinder_1004 as tk
 #find words that match the beginning of other words. or the end?
 # initial clipping, final clipping, medial clipping
 
-
+#procedure is
+# 1. clippingfinder - ID potnetial clippings
+# 2. picker - make yes/no list
+# 3. sampler - add context to yeslist
 
 with codecs.open('/Users/ps22344/Downloads/chapter2/outputfiles/fulldict_1115.json', 'r', 'utf-8') as jsoninput:
 	fulldict=json.load(jsoninput)
@@ -35,6 +38,8 @@ def picker(dictionary, output_name):
 	with codecs.open("picker_yes_"+output_name+".json", "w", "utf-8") as writejson:
 		json.dump(outputdict, writejson)
 	
+	
+#picker(fulldict, "final_3chars_")	
 #use picker to separate good from evil. write yeslist to json
 
 #then run the yeslist thru the tokenfinder
@@ -50,7 +55,7 @@ def clippingfinder(dictionary, cutoff):
 	print "len dict before", len(dictionary)
 	dictionary = {k:v for k,v in dictionary.items() if v > cutoff}
 	print "len dict after", len(dictionary)
-	for length in [3]:
+	for length in [4]:
 		print length
 		for entry in set([i for i in dictionary.keys() if not "/" in i]):
 			if len(entry) < length + 3:
@@ -60,7 +65,7 @@ def clippingfinder(dictionary, cutoff):
 				for item in set([i for i in dictionary.keys() if i != entry and not "/" in i]):
 					if item == entry[:length]:
 						outputdict[item].append(entry)
-	picker(outputdict, "3chars_1116")
+	picker(outputdict, "4chars_1116")
 	# for finding in outputdict.keys():
 # 		print "\n", finding, outputdict[finding]
 	os.system('say "your program has finished"')
@@ -70,23 +75,26 @@ def clippingfinder(dictionary, cutoff):
 # 		print the list. 
 # 		evtl tokenfind it
 
-#clippingfinder(fulldict, 5)
+clippingfinder(fulldict, 5)
 
 
 def sampler(json_input, output_name):
+	
+	sampledict=defaultdict(list)
 	with codecs.open(json_input, 'r', 'utf-8') as jsoninput:
 		fulldict=json.load(jsoninput)
 		print "length of dicti", len(fulldict)
 
 	for entry in fulldict.keys():
-		sampledict=defaultdict(list)
+		
 		print entry
 		samples=tk.tokenfinder(["\W"+entry+"\W"], "/Users/ps22344/Downloads/craig_0208", length=20)
 		sampledict[entry]=samples
 	with codecs.open("sampler_yes_"+output_name+".json", "w", "utf-8") as writejson:
 		json.dump(sampledict, writejson)
+	print "written to ", writejson
 
-sampler('/Users/ps22344/Downloads/chapter2/current/picker_yes_3chars_1116.json', "3chars_1116")
+#sampler('/Users/ps22344/Downloads/chapter2/current/picker_yes_3chars_1116.json', "3chars_1116")
 	
 
 
