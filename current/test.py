ff43ab97d017dc1f99cf8defebb37dab3b381267
patrick <patrickschultz@utexas.edu>
2016-11-17 14:54:50 -0600

    moving files to appropriate folder

--- current/test.py
+++ current/test.py
@@ -1,8 +1,11 @@
 import json
 import codecs
 
-
+sdsd
 with codecs.open('/Users/ps22344/Downloads/chapter2/current/shorteningdict_2_1115.json', 'r', 'utf-8') as jasi:
 	dict=json.load(jasi)
 
-print dict
+d={"a":{"sub":[], "sub2":[1000,2000,3000]}, "b":{"sub":[40], "sub2":[]}}
+
+e={"a":{"sub":[1,2,3], "sub2":[1000,2000,3000]}, "b":{"sub":[20], "sub2":[]}}
+
