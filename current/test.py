995307583c171a0cb21ce66ca4684679c12ed4fd
patrick <patrickschultz@utexas.edu>
2016-11-17 10:18:33 -0600

    get out of my way test py

--- current/test.py
+++ current/test.py
@@ -1,11 +1,8 @@
-import re
-import nltk
-from nltk import pos_tag, word_tokenize
-
-texti=["I", "fought", "the", "law"]
-
-print pos_tag(texti)
-print pos_tag("I")
+import json
+import codecs
 
 
+with codecs.open('/Users/ps22344/Downloads/chapter2/current/shorteningdict_2_1115.json', 'r', 'utf-8') as jasi:
+	dict=json.load(jasi)
 
+print dict
