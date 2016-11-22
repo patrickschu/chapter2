c0b8f687eaeb6c4ccf74e46b680f6e809f5e9154
patrick <patrickschultz@utexas.edu>
2016-11-22 15:39:38 -0600

    fix faulty entries

--- current/test.py
+++ current/test.py
@@ -6,5 +6,14 @@ tt="lol(kid)"
 
 #print tt[-3:-1]
 
-print re.sub("[\(\)]", "XXXXX", tt)
+#print re.sub("[\(\)]", "XXXXX", tt)
+
+
+p1=(1,2)
+p2=(4,4)
+
+def dist(pt1, pt2):
+	print abs(pt1[0]-pt2[0]) + abs(pt1[1]-pt2[1])
+	
+dist(p1,p2)
 
