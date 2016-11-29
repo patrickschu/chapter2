71a087f0e4b52d2de2ec58ed950e5e69dc75da19
patrick <patrickschultz@utexas.edu>
2016-11-29 09:00:30 -0600

    we take it from here tomorrow

--- current/characterfinder_1122.py
+++ current/characterfinder_1122.py
@@ -25,7 +25,9 @@ print string.ascii_letters
 	
 #u2 uR ub UC ru  2b c ya
 
-alphabet = {k:0 for k in [i for i in list(string.ascii_letters) if not i in ["Q", "q", "P", "p",  "I", "i", "M", "m", "L", "l", "g", "j", "v", "s", "t", "h", "H", "K", "k", "A", "a", "E", "e", "Z", "z"]]}
+alphabet = {k:0 for k in [i for i in list(string.ascii_letters) if not i in [
+"D", "d", "F", "f", "G", "J", "Q", "q", "P", "p",  "I", "i", "M", "m", "O", "o", "L", "l", "g", "j", "V", "v", "s", "S", "t", "h", "H", "K", "k", "A", "a", "E", "e", "Z", "z"
+]]}
 print "these are our search terms", alphabet
 excludelist=[]
 
@@ -57,3 +59,36 @@ def characterfinder(input_dir, input_dict):
 	return results 
 		
 characterfinder( "/Users/ps22344/Downloads/craig_0208", alphabet)
+
+counterdict={
+"xX"=re.compile("(Xx)
+
+
+}
+
+
+def charactercounter(input_dir, input_dict):
+	results=[]
+	dicti=defaultdict(float)
+	matchesdicti=defaultdict(list)
+	for entry in input_dict:
+		print entry
+	characterlist=set([i for i in input_dict.keys()])
+	print [i.pattern for i in characterlist]
+	for dir in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
+		print dir
+		for fili in [i for i in os.listdir(os.path.join(input_dir, dir)) if not i.startswith(".")]:
+			with codecs.open(os.path.join(input_dir, dir, fili), "r", "utf-8") as inputtext:
+				inputad=ct.adtextextractor(inputtext.read(), fili)
+			#result is a list of lists which contain matches for each regex/acronym
+			result=[([m for m in i.findall(inputad) if not m in excludelist], i.pattern) for i in characterlist] 
+			results.append([len(matches) for matches, pattern in result])
+			for matches, pattern in result:
+				#the dicti is {pattern:count, pattern: count, ...}
+				dicti[pattern]=dicti[pattern]+len(matches)
+				matchesdicti[pattern]=matchesdicti[pattern]+matches
+	print "\n".join([":".join((i, str(dicti[i]), "|".join(set(matchesdicti[i])))) for i in sorted(dicti, key=dicti.get, reverse=True)])	
+	for entry in {k:v for k,v in matchesdicti.items() if v > 10}:
+		print entry
+		tk.tokenfinder([re.sub("[\(\)]", "", entry)], "/Users/ps22344/Downloads/craig_0208", lower_case=False)
+	return results 
