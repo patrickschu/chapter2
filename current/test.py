2198589fcf6cd4a29bb75d93e8a8ab2b86887a6d
U-laptop\ps <patrickschultz@utexas.edu>
2016-12-16 19:12:46 +0100

    add normalized and raw data output

--- current/test.py
+++ current/test.py
@@ -37,14 +37,16 @@ def repeatedpunctuationfinder(dir):
 	search_terms=punctuationdict.keys()
 	for pati in [i for i in os.listdir(dir) if not i.startswith(".")]:
 		print pati
-		for fili in [i for i in os.listdir(os.path.join(dir, pati)) if not i.startswith(".")]:
+		for fili in [i for i in os.listdir(os.path.join(dir, pati)) if not i.startswith(".")][:2]:
 			with codecs.open(os.path.join(dir, pati, fili), "r", "utf-8") as inputfili:
 				inputad=ct.adtextextractor(inputfili.read(), fili)
-			wordcount=len(ct.tokenize(inputad))
+			wordcount=float(len(ct.tokenizer(inputad)))
+			print wordcount
 			result=[k.findall(inputad) for k in search_terms]
 			for no, item in enumerate(result):
 				resultdict[no]=resultdict[no]+len(item)
 			results.append([(len(i), len(i)/wordcount) for i in result])
+			print results
 			if sum([len(i) for i in result]) > 80:
 				for n, i in enumerate(result):
 					 if len(i) > 0:
@@ -62,11 +64,16 @@ def repeatedpunctuationfinder(dir):
 		print k, resultdict[k]
 	print "shape of results, number of lists:", len(results),  "-- length of lists", set([len(i) for i in results])
 	#1st list is absolute counts, 2nd div by word count
-	return [i[0] for i in results], [i[1] for i in results]
+	#for g in [[x[1] for x in i] for i in results]:
+	#	print g
+	#for g in [i[1] for i in results]:
+	#	print g
+	return [[x[0] for x in i] for i in results], [[x[1] for x in i] for i in results]
 
-repeatedpunctuationfinder("/home/ps22344/Downloads/craig_0208")
+x, y=repeatedpunctuationfinder("/home/ps22344/Downloads/craig_0208")
 	
-
+print y
+	
 def emoticonfinder(dir):
 	"""
 	The emoticonfinder takes a directory with corpus files as input. 
