c011fd757f23f0d5ab28c18a2a99a16ade92884b
U-laptop\ps <patrickschultz@utexas.edu>
2016-12-16 19:16:01 +0100

    copy over punctuationfinder; add snarky comment to emoticonfinder

--- current/egrammartools.py
+++ current/egrammartools.py
@@ -25,6 +25,7 @@ import json
 
 def emoticonfinder(dir):
 	"""
+	THIS STILL NEEDS THE OUTPUT FUNCTION OF PUNCTFINDER
 	The emoticonfinder takes a directory with corpus files as input. 
 	It returns a list of lists with counts of each emoticon in each file in dir.
 	Emoticons are read from file.
@@ -103,6 +104,55 @@ def emoticonfinder(dir):
 
 
 def repeatedpunctuationfinder(dir):
+	"""
+	The repeatedpunctuationfinder uses string.punctuation to create a dictionary of regexes.
+	These are used to identify non-Standard usage of punctuation. 
+	Note how we hardcoded the ?!? variants into the punctuationdict from the start. 
+	Iterates over files contained in dir.
+	Returns two lists; first is absolute counts, the second relative counts	
+	-- Source file is /Users/ps22344/Downloads/chapter2/current/punctuationcounter_0927.py ---
+	"""
+	starttime=time.time()
+	#creating a featuredict from file
+	results=[]
+	resultdict=defaultdict(float)
+	punctuationdict={
+	re.compile(r"(?:\s|\w)(!\?|\?!)(?:\s|\w)"):0
+	}
+
+	for stringi in punctuation:
+		print stringi, "-->", re.escape(stringi)
+		punctuationdict[re.compile("((?:"+re.escape(stringi)+" ?){2,})")]=0	
+	search_terms=punctuationdict.keys()
+	for pati in [i for i in os.listdir(dir) if not i.startswith(".")]:
+		print pati
+		for fili in [i for i in os.listdir(os.path.join(dir, pati)) if not i.startswith(".")]:
+			with codecs.open(os.path.join(dir, pati, fili), "r", "utf-8") as inputfili:
+				inputad=ct.adtextextractor(inputfili.read(), fili)
+			wordcount=float(len(ct.tokenizer(inputad)))
+			result=[k.findall(inputad) for k in search_terms]
+			for no, item in enumerate(result):
+				resultdict[no]=resultdict[no]+len(item)
+			results.append([(len(i), len(i)/wordcount) for i in result])
+			print results
+			if sum([len(i) for i in result]) > 80:
+				for n, i in enumerate(result):
+					 if len(i) > 0:
+						 print search_terms[n].pattern, n,i, "len", len(i)
+				print os.path.join(dir, pati, fili), "\n"
+				os.system("cygstart "+os.path.join(dir, pati, fili))
+			
+	endtime=time.time()
+	print "This took us {} minutes".format((endtime-starttime)/60)
+	#print results
+	print len(results), "files processed"
+	print "\n\n"
+	resultdict={search_terms[k].pattern:v for k,v in resultdict.items() if v > 0}
+	for k in sorted(resultdict, key=resultdict.get, reverse=True):
+		print k, resultdict[k]
+	print "shape of results, number of lists:", len(results),  "-- length of lists", set([len(i) for i in results])
+	#1st list is absolute counts, 2nd div by word count
+	return [[x[0] for x in i] for i in results], [[x[1] for x in i] for i in results]
 	"""
 	The repeatedpunctuationfinder uses string.punctuation to create a dictionary of regexes.
 	These are used to identify non-Standard usage of punctuation. 
