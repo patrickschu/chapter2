83f87eefa5f7b8fa3aafd4ffd4ce749882e79fb9
U-laptop\ps <patrickschultz@utexas.edu>
2016-12-17 22:41:23 +0100

    paste rebusfinder 2 and change name of rebus finder 4

--- current/egrammartools.py
+++ current/egrammartools.py
@@ -200,7 +200,7 @@ def leetcounter(dir):
 
 
 #the rebusfinder needs to be here; it finds instances of "4" for "for. 
-def rebusfinder(input_path):
+def rebusfinder_for(input_path):
 	"""
  	This finds words that are represented as numbers. 
  	All combinations \W([a-z]+)\s+("+unicode(number)+")\s+([a-z]+)\W for the number put in are identified.
@@ -268,6 +268,121 @@ def rebusfinder(input_path):
 
 # the rebusfinder 2 needs to be here; it finds instances of "2" for "to".
 # /Users/ps22344/Downloads/chapter2/current/identifying_rebus_2_1012.py
+def rebusfinder_to(input_dir):
+	"""
+ 	This finds the word "to"  that represented as the number 2. 
+ 	All combinations \W([a-z]+)\s+("+unicode(number)+")\s+([a-z]+)\W for the number put in are identified.
+ 	The lists exclude_pre_context and exclude_post_context exclude instances where a word follows (post) or precedes (pre) the "2" per regex. 
+ 	Procedure: 
+ 	Eliminate all pre and post contexts;
+ 	POS tag the remaining ones and eliminate certain combinations;
+ 	Find positives by POS tag and a word list;
+ 	Dismiss the remaining ones. 
+ 	It returns a list of positives. 
+ 	It print the results and give type and token counts. 
+	Returns a list of lists where each list contains raw and per word counts. 
+	"""
+	nounregex=re.compile("NN.?")
+	exclude_post_context=["inche?s?", "wks?", "nd", "i", "sec", "stepping", "asap", "tattoos", "years?", "yrs", "months?", "weeks?", "days?", "hours?", "times?", "peoples?", "(boy|girl)?friends?", "(p|a)\.?m\.?", "to", "or", "full", "wana" ]
+	exclude_post_context=[re.compile(r"^"+i+"$") for i in exclude_post_context]
+	exclude_pre_context=["borderlands", "chicago", "c?ops?", "till?", "or", "and", "just", "f(oo|ee)?t", "last", "own", "hsv", "herpes"]
+	exclude_pre_context=[re.compile(r"^"+i+"$") for i in exclude_pre_context]
+	search_terms=[2]
+	for number in search_terms:
+		numberregex=re.compile("\W([a-z]+)\s+("+unicode(number)+")\s+([a-z]+)\W")
+		#just for now
+		h0dict=defaultdict(int)
+		h2dict=defaultdict(int)
+		print numberregex.pattern
+		results=[]
+		for pati in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
+			for fil in [i for i in os.listdir(os.path.join(input_dir, pati)) if not i.startswith(".")]:
+				result=[]
+				fili=codecs.open(os.path.join(input_dir, pati, fil), "r", "utf-8")
+				inputad=ct.adtextextractor(fili.read(), fil)
+				inputad=inputad.lower()
+				wordcount=float(len(ct.tokenizer(inputad)))
+				hits=numberregex.findall(inputad)
+				#this weeds out all the phonenumbers. 
+				hits=[h for h in hits if h[0] not in writtennumberdict and h[2] not in writtennumberdict]
+				for h in hits:
+					#print  h
+					if not any (regex.match(h[2]) for regex in exclude_post_context) and not any (regex.match(h[0]) for regex in exclude_pre_context):
+						tagged=pos_tag(h)
+						#taking out trash
+						if (
+							(tagged[0][1] in ["DT", "JJS", "TO", "PRP$"]) 
+							or
+							(tagged[0][1]=="IN" and h[0] not in ["out", "like"])
+							or
+							(tagged[0][1] in ["VBG"] and h[0] not in ["talking", "responding", "waiting", "getting","looking", "going", "trying"])
+							or
+							(tagged[0][1] in ["VB", "VBD", "VBP", "VBZ"] and tagged[2][1] in ["JJ"])
+							or
+							#this is where we screw up
+							(tagged[2][1] in ["NNS"] and h[2] not in ["chat", "kiss", "go", "know", "find", "do", "c", "knees"])
+							or
+							(tagged[2][1]=="IN")
+							or
+							(tagged[2][1]=="CC" and h[2] not in ["but"])
+							or
+							#we don't need this if we are to just ignore whatever goes thru all of it
+							#TEMPTEMPTEMP
+							(h[0] in ["be", "other", "s", "type", "was", "work", "im", "baths", "you", "maybe", "big", "day", "o", "round", "ride", "avengers", "kids", "had", "number", "have", "like", "here", "size", "got", "are", "send", "only", "have", "go", "is", "bedroom", "but", "beautiful", "nice"])
+							or
+							(h[2] in ["face", "new", "faced", "wonderful", "must", "min", "short", "si", "br", "step", "start", "so", "out", "story", "bdrm", "other", "out", "story", "yr", "looking", "more", "but", "hrs", "bedroom"])
+							or 
+							(tagged[2][1] in ["JJ", "VBD", "VBZ", "VBG"])
+							):
+							#print "killed",tagged, "\n"
+							pass
+						
+						#finding the good
+						elif (
+							(tagged[2][1] in ["DT", "CD", "EX", "NNS", "VB"])
+							or
+							(tagged[2][1] in ["JJ"] and h[0] in ["opposed"])
+							or
+							(tagged[2][1] in ["PRP"] and not nounregex.match(tagged[0][1]))
+							or
+							(h[0] == "have" and h[2] in ["browse", "force", "go", "send", "talk"])
+							or
+							(h[0] == "like" and h[2] not in ["furry", "cuz", "straight"])
+							or
+							(h[0] in ["here"] and nounregex.match(tagged[2][1]))
+							or
+							#really what we are exluding here is anything non-Verb or Noun
+							# we can consider replacing this with a regex
+							(h[0] in ["need", "me", "pics"] and tagged[2][1] not in ["JJ", "JJR", "MD"])
+							or 
+							(h[0] in ["momma", "women", "delighted", "tryn", "respond", "travel", "veldkum", "happness", "pool", "lots", "bbw", "willin", "luvz", "place", "time", "married", "pixs", "boy", "pictures", "brickz", "somebody", "memphis", "cell", "fear", "hoop", "open", "goes", "afraid", "speak", "lady", "needs", "attracted", "doms", "bottom", "head", "apply", "drive", "pic", "newer", "pinned", "luvs", "sumbody", "face", "due", "tryin", "line", "has", "close", "interested", "alot", "oral", "talk", "new", "girl", "up", "scared", "willing", "cam", "loves", "cock", "out", "u", "nice", "how", "free", "hard", "hope", "able", "someone", "man", "woman", "male", "down", "love", "luv", "ready", "want", "wants"]+["talking", "responding", "waiting", "getting","looking", "lookin", "going", "trying"])
+							or
+							(h[2] in ["survive", "brag", "blow", "grab", "feel", "send", "connect", "hearing", "say", "read", "contact", "please", "run", "host","kno", "talk", "just", "add", "text", "chill", "hang", "date", "find", "chat", "show", "u", "meet", "her", "hear", "me", "my", "b", "know", "play", "do", "suck", "go", "get", "fuck"])
+							):
+							#print "hooked the plusloop", tagged
+							#print fil, "len tagged", type(tagged)
+							result.append(tagged)
+							#print "len result", len(result)
+							h0dict[h[0]]=h0dict[h[0]]+1
+ 							h2dict[h[2]]=h2dict[h[2]]+1
+						else:
+							pass
+				results.append([(len(result), len(result)/wordcount)])
+				#print [(len(result), len(result)/wordcount)]
+		print "We have {} items with a token count of {}".format(len(h0dict.keys()), sum(h0dict.values()))
+		h0dict={k:v for k,v in h0dict.items() if v > 3}
+		h2dict={k:v for k,v in h2dict.items() if v > 3}
+		print "\n\n", number, "\npretext here be the results\n\n"
+		for entry in sorted(h0dict, key=h0dict.get, reverse=True):
+			print entry, h0dict[entry]
+		print "We have {} post items with a token count of {}".format(len(h2dict.keys()), sum(h2dict.values()))
+		for entry in sorted(h2dict, key=h2dict.get, reverse=True):
+			print entry, h2dict[entry]
+		print "We have {} pre items with a token count of {}".format(len(h0dict.keys()), sum(h0dict.values()))
+		#print [i for i in results if sum(i[0]) > 2]
+		#print results
+		print "shape of results, number of lists:", len(results),  "-- length of lists", set([len(i) for i in results])
+		return [[x[0] for x in i] for i in results], [[x[1] for x in i] for i in results]
 
 
 
