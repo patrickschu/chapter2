8743468d7c75e68734057edb7001ece139ea6bd7
U-laptop\ps <patrickschultz@utexas.edu>
2016-12-19 17:03:06 +0100

    fix references to arguments, fix output formatting

--- current/test.py
+++ current/test.py
@@ -10,43 +10,71 @@ import json
 from nltk import pos_tag
 
 
-import time
-import codecs
-import json
-import re
-import os
-import clustertools as ct
-import tokenfinder_1004 as tk
-from collections import defaultdict
-from nltk import pos_tag
-from string import punctuation
-print punctuation
+
+def acronymcounter(input_dir):
+	"""
+	The acronymcounter uses the acronym_list to count instances	of the abbreviations listed in there. 
+	Here, we make that list out of the shorteningdict jsons created earlier. 
+	The regex is designed to find lowercase and uppercase versions of each, plus plurals.
+	The input_dir contains the text files to be iterated over. 
+	It returns a list of match counts.
+	e.g.
+	acronym_list=['LOL', 'ROFL', 'ASL', 'BRB']
+	result=[0,0,2,0] 
+	NOTE:we can consider running location and schools over a different regex that does not include plural s.
+	"""
+	filelist=[
+	"abbreviationfiles/shorteningdict_2_1115.json",
+	"abbreviationfiles/shorteningdict_3_1115.json",
+	"abbreviationfiles/shorteningdict_4_1115.json",
+	"abbreviationfiles/shorteningdict_5_1115.json",
+	"abbreviationfiles/shorteningdict_6_1115.json"
+	]
+
+	search_terms = []
+
+	for fili in filelist:
+		with codecs.open(fili, "r", "utf-8") as inputfile:
+			acronym_dict=json.load(inputfile)
+		for key in [i for i in acronym_dict.keys() if i not in ["blend", "abbreviation", "clipping", "delete", "other"]]:
+			for cat in ['X', 'location']:
+				print "adding", acronym_dict[key][cat]
+				search_terms = search_terms + acronym_dict[key][cat]
+
+	print search_terms
+			
+	print "we have {} search terms".format(len(search_terms))
+		
+	excludelist=set(["oks", "fbs", "PSS", "VAS", "vas", "BCS", "bcs", "NES", "nes", "SMS", "sms", "SAS", "SSS", "sss", "nsas", "mias"])
+	
+	#dicts to store results
+	dicti=defaultdict(float)
+	matchesdicti=defaultdict(list)
+	results=[]
+	
+	#regex, lower and pluralize
+	acronym_list=[re.compile("\W((?:"+i+"|"+i.lower()+")[sS]?)\W") for i in search_terms]
+	acronym_list=set(acronym_list)
+	print [i.pattern for i in acronym_list]
+	#iterate and match
+	for dir in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
+		print dir
+		for fili in [i for i in os.listdir(os.path.join(input_dir, dir)) if not i.startswith(".")][:20]:
+			with codecs.open(os.path.join(input_dir, dir, fili), "r", "utf-8") as inputtext:
+				inputad=ct.adtextextractor(inputtext.read(), fili)
+			#result is a list of lists which contain matches for each regex/acronym
+			wordcount=float(len(ct.tokenizer(inputad)))
+			result=[([m for m in i.findall(inputad) if not m in excludelist], i.pattern) for i in acronym_list] 
+			results.append([(len(matches), len(matches)/wordcount) for matches, pattern in result])
+			for matches, pattern in result:
+				#the dicti is {pattern:count, pattern: count, ...}
+				dicti[pattern]=dicti[pattern]+len(matches)
+				matchesdicti[pattern]=matchesdicti[pattern]+matches
+	print "\n".join([":".join((i, str(dicti[i]), "|".join(set(matchesdicti[i])))) for i in sorted(dicti, key=dicti.get, reverse=True)])	
+	return results 
 
 
-import clustertools as ct
-import tokenfinder_1004 as tk
-import re
-import codecs
-from collections import defaultdict
-import os
-
-#this is based on the acronymcounter. 
-# files have been exchanged
-# - check out all variatns
-#  -- exclude some contexts cf notes
-#  - investigate plurals
-#  - remove duplicates 
-
-import numpy as np
-import os
-import json
-import codecs
-import re
-import clustertools as ct
-from collections import defaultdict
-import tokenfinder_1004 as tk
-
-
+x=acronymcounter("/home/ps22344/Downloads/craig_0208")
  
 
 def clippingcounter(input_dir):
@@ -86,8 +114,8 @@ def clippingcounter(input_dir):
 					print "adding", acronym_dict[key][cat]
 					search_terms = search_terms + [i if i in ["loc", "les", "sis"] else i + "s?" for i in acronym_dict[key][cat]]
 
-	for i in search_terms:
-		print i, search_terms.count(i)
+	#for i in search_terms:
+	#	print i, search_terms.count(i)
 			
 	print "we have {} search terms".format(len(search_terms))
 	print "we have {} set search terms".format(len(set(search_terms)))
@@ -108,7 +136,7 @@ def clippingcounter(input_dir):
 	#iterate and match
 	for dir in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
 		print dir
-		for fili in [i for i in os.listdir(os.path.join(input_dir, dir)) if not i.startswith(".")]:
+		for fili in [i for i in os.listdir(os.path.join(input_dir, dir)) if not i.startswith(".")][:5]:
 			with codecs.open(os.path.join(input_dir, dir, fili), "r", "utf-8") as inputtext:
 				inputad=ct.adtextextractor(inputtext.read(), fili).lower()
 			#result is a list of lists which contain matches for each regex/acronym
@@ -135,6 +163,7 @@ def clippingcounter(input_dir):
 	#	print u
 	return [[x[0] for x in i] for i in results], [[x[1] for x in i] for i in results] 
 
+#clippingcounter("/home/ps22344/Downloads/craig_0208")
 
 def charactercounter(input_dir):
 	"""
