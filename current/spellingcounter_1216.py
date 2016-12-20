a04128965aa1a2f180bb71ddc637a147ea32056f
U-laptop\ps <patrickschultz@utexas.edu>
2016-12-20 22:54:04 +0100

    experiment with different sets/lists for exclusion of good words

--- current/spellingcounter_1216.py
+++ current/spellingcounter_1216.py
@@ -21,11 +21,12 @@ def spellingcounter(input_dir):
     It returns a lists of lists with (raw count, relative count) tuples.
     """
     start=time.time()
-    html.regex=re.compile("<.*?>")
+    goodwords=set(["wo", "'ve", "'m", "n't", "'s", "'ll"]+list(string.punctuation))
+    htmlregex=re.compile("<.*?>")
     results=[]
     for pati in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
         print pati
-        for fili in [i for i in os.listdir(os.path.join(input_dir, pati)) if not i.startswith(".")][:5]:
+        for fili in [i for i in os.listdir(os.path.join(input_dir, pati)) if not i.startswith(".")][:200]:
             print fili
             result=[]
             fili=codecs.open(os.path.join(input_dir, pati, fili), "r", "utf-8")
@@ -34,7 +35,7 @@ def spellingcounter(input_dir):
             words=ct.tokenizer(inputad)
             #print "\n\n\n", words
             wordcount=float(len(words))
-            mistakes=[w for w in words if not spellchecker(w) and w not in ["wo", "'ve", "'m", "n't", "'s", "'ll"]+list(string.punctuation)]
+            mistakes=[w for w in words if not spellchecker(w) and w not in goodwords]
             #print result, len(result)
             if wordcount-len(mistakes) < 0:
                  print "WARNING: negative count-mistakes", wordcount, len(correct), os.path.join(input_dir, pati, fili)
@@ -42,7 +43,7 @@ def spellingcounter(input_dir):
             #print "\n".join([":".join([i, str(dict[i])]) for i in sorted(dict, key=dict.get, reverse=True)])
     end=time.time()
     print "len results", len(results)
-    print "this took us {} minutes".format(end-start/60)
+    print "this took us {} minutes".format((end-start)/60)
     print "shape of results, number of lists:", len(results),  "-- length of lists", set([len(i) for i in results])
     #for u in [[x[1] for x in i] for i in results]:
     #    print u
