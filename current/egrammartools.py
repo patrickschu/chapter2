a6c86d093156950d54fdeeacf7a3b94127530282
U-laptop\ps <patrickschultz@utexas.edu>
2016-12-21 23:35:58 +0100

    paste prosodycounter and anyoftheseregexes

--- current/egrammartools.py
+++ current/egrammartools.py
@@ -845,6 +845,93 @@ def acronymcounter(input_dir):
 
 #spellings representing prosody or non-linguistic sounds such as laughter 
 
+	"""
+	The anyofthesregex iterates over all instances with "+" in a regex to construct a new pattern.
+	THe new pattern replaces one instance of + with a {2,}. 
+	Thus, this will get us a string to match Hhello, Heeeeello but not Hheello.
+	"""
+	print "we run the anyoftheseregex on", regexstring
+	#print regexstring.split("+")
+	result=[i for i in regexstring.split("+") if i]
+	outputregex=[]
+	for number, item in enumerate(result):
+		temp=[i for i in regexstring.split("+") if i]
+		temp[number]=item+"{2,}"
+		outputregex.append(temp)
+		
+	anyregex=")|(?:".join(["".join(i) for i in outputregex])
+	print "((?:"+anyregex+"))"
+	return "((?:"+anyregex+"))"
+	
+
+def prosodycounter(input_dir):
+	"""
+	 
+	Returns a list of lists where each list contains raw and per word counts.
+	
+	"""
+	start=time.time()
+	
+	#creating the search terms
+	prosodyitems=[
+	"\s(\*(?:laugh|cough|smack|giggle)\*)\s",
+
+	"\W([Ee][Rr])\W",
+
+	"\W((?:[Hh][Aa]){1,}[Hh]?)\W",
+	"\W((?:[Hh][Uu]){1,}[Hh]?)\W",
+	"\W((?:[Hh][Ee]){2,}[Hh]?)\W",
+	"\W([Hh][Oo]{2,})\W",
+	"\W([Hh][Mm]{1,})\W",
+
+	"\W([Hh]e+y{2,})\W",
+	"\W([Hh]e{2,}[Yy]+)\W",
+	"\W"+anyoftheseregex("[Hh]+[Ee]+[Ll][Ll]+[Oo]+")+"\W",
+
+	"\W([Mm]{2,}[Hh]?)\W",
+	"\W((?:[Mm][Hh]){1,})\W",
+
+	"\W([Ss][Oo]{2,})\W",
+
+	"\W([Uu][Hh]+)\W",
+	"\W([Uu][Mm]+)\W",
+
+	"\W([Yy][Aa]+[Yy]+)\W",
+	"\W([Yy]+[Aa]+[Hh]?)\W"
+	]
+	excludelist=[]
+	
+	#dicts to store results
+	dicti=defaultdict(float)
+	matchesdicti=defaultdict(list)
+	results=[]
+	
+	prosody_list=[re.compile(i) for i in prosodyitems]
+	print "{} items in the prosody_list, {} unique".format(len(prosody_list), len(set(prosody_list)))
+	print [i.pattern for i in prosody_list]
+	#iterate and match
+	for dir in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
+		print dir
+		for fili in [i for i in os.listdir(os.path.join(input_dir, dir)) if not i.startswith(".")]:
+			with codecs.open(os.path.join(input_dir, dir, fili), "r", "utf-8") as inputtext:
+				inputad=ct.adtextextractor(inputtext.read(), fili).lower()
+			#result is a list of lists which contain matches for each regex/acronym
+			wordcount=float(len(ct.tokenizer(inputad)))
+			result=[([m for m in i.findall(inputad) if not m in excludelist], i.pattern) for i in prosody_list] 
+			#print result
+			results.append([(len(matches), len(matches)/wordcount) for matches, pattern in result])
+			for matches, pattern in result:
+				#print pattern
+				#the dicti is {pattern:count, pattern: count, ...}
+				dicti[pattern]=dicti[pattern]+len(matches)
+				matchesdicti[pattern]=matchesdicti[pattern]+matches
+	print "\n".join([":".join((i, str(dicti[i]), "|".join(set(matchesdicti[i])))) for i in sorted(dicti, key=dicti.get, reverse=True)])
+	end=time.time()
+	print "This took us {} minutes".format((end-start)/60)
+	# for u in [[x[0] for x in i] for i in results]:
+		# print u
+	print "shape of results, number of lists:", len(results),  "-- length of lists", set([len(i) for i in results])
+	return [[x[0] for x in i] for i in results], [[x[1] for x in i] for i in results] 
 
 
 
@@ -856,3 +943,23 @@ def acronymcounter(input_dir):
 
 ###SECTION 4
 ###UTTERANCE LEVEL
+
+#helper funcs
+def anyoftheseregex(regexstring):
+	"""
+	The anyofthesregex iterates over all instances with "+" in a regex to construct a new pattern.
+	THe new pattern replaces one instance of + with a {2,}. 
+	Thus, this will get us a string to match Hhello, Heeeeello but not Hheello.
+	"""
+	print "we run the anyoftheseregex on", regexstring
+	#print regexstring.split("+")
+	result=[i for i in regexstring.split("+") if i]
+	outputregex=[]
+	for number, item in enumerate(result):
+		temp=[i for i in regexstring.split("+") if i]
+		temp[number]=item+"{2,}"
+		outputregex.append(temp)
+		
+	anyregex=")|(?:".join(["".join(i) for i in outputregex])
+	print "((?:"+anyregex+"))"
+	return "((?:"+anyregex+"))"
