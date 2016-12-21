e53c31179ae407d6fe2f25b02729db66aff4b6b6
U-laptop\ps <patrickschultz@utexas.edu>
2016-12-21 12:25:12 +0100

    add the spellingcounter to calculate rate of misspellings

--- current/egrammartools.py
+++ current/egrammartools.py
@@ -635,7 +635,44 @@ def singleletterfinder(input_dir):
 ###ORTHOGRAPHY
 
 #spelling correctness?
-
+def spellingcounter(input_dir):
+    """
+    The spellingcounter counts the number of mis-spelled words.
+    It uses the PyEnchange library for spellchecking.
+    It iterates over the files in input_dir.
+    It returns a lists of lists with (raw count, relative count) tuples.
+	Based on spellingcounter_1216
+    """
+    start=time.time()
+    americandict = enchant.Dict("en_US")
+    goodwords=set(["wo", "'ve", "'m", "n't", "'s", "'ll", "'re", "'d", "non-"]+list(string.punctuation))
+    htmlregex=re.compile("<.*?>")
+    results=[]
+    for pati in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
+        print pati
+        for fili in [i for i in os.listdir(os.path.join(input_dir, pati)) if not i.startswith(".")]:
+            #print fili
+            result=[]
+            fili=codecs.open(os.path.join(input_dir, pati, fili), "r", "utf-8")
+            inputad=ct.adtextextractor(fili.read(), fili)
+            inputad=htmlregex.sub(" ", inputad)
+            words=ct.tokenizer(inputad)
+            #print "\n\n\n", words
+            wordcount=float(len(words))
+            mistakes=[w for w in words if not americandict.check(w) and w not in goodwords]
+            #print mistakes
+            if wordcount-len(mistakes) < 0:
+                 print "WARNING: negative count-mistakes", wordcount, len(correct), os.path.join(input_dir, pati, fili)
+            results.append([(len(mistakes), len(mistakes)/wordcount)])
+            #print "\n".join([":".join([i, str(dict[i])]) for i in sorted(dict, key=dict.get, reverse=True)])
+    end=time.time()
+    print "len results", len(results)
+    print "this took us {} minutes".format((end-start)/60)
+    print "shape of results, number of lists:", len(results),  "-- length of lists", set([len(i) for i in results])
+    #for u in [[x[1] for x in i] for i in results]:
+    #    print u
+    #print [[x[0] for x in i] for i in results], [[x[1] for x in i] for i in results]
+    return [[x[0] for x in i] for i in results], [[x[1] for x in i] for i in results]
 
 #clippings
 def clippingcounter(input_dir):
