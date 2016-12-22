c8303a0ad226c8c8b0099f41220774197d94a338
U-laptop\ps <patrickschultz@utexas.edu>
2016-12-23 00:43:32 +0100

    we run the whole thing for the first time. exciting!

--- current/03_analysis_egrammar_1220.py
+++ current/03_analysis_egrammar_1220.py
@@ -5,9 +5,6 @@ import time
 completestart=time.time()
 listi=[]
 
-emos_raw, emos_freq=eg.emoticonfinder('/home/ps22344/Downloads/craig_0208')
-listi.append(np.array(emos_freq))
-
 rep_raw, rep_freq= eg.repeatedpunctuationfinder('/home/ps22344/Downloads/craig_0208')
 listi.append(np.array(rep_freq))
 
@@ -19,23 +16,26 @@ listi.append(np.array(leet_freq))
 rebfor_raw, rebfor_freq= eg.rebusfinder_for('/home/ps22344/Downloads/craig_0208')
 listi.append(np.array(rebfor_freq))
 
-# rebto_raw, rebto_freq= eg.rebusfinder_to('/home/ps22344/Downloads/craig_0208')
-# listi.append(np.array(rebto_freq))
+rebto_raw, rebto_freq= eg.rebusfinder_to('/home/ps22344/Downloads/craig_0208')
+listi.append(np.array(rebto_freq))
 
-# rebtoo_raw, rebtoo_freq= eg.rebusfinder_too('/home/ps22344/Downloads/craig_0208')
-# listi.append(np.array(rebtoo_freq))
+rebtoo_raw, rebtoo_freq= eg.rebusfinder_too('/home/ps22344/Downloads/craig_0208')
+listi.append(np.array(rebtoo_freq))
 
-# caps_raw, caps_freq=eg.capsfinder('/home/ps22344/Downloads/craig_0208', 0.5)
-# listi.append(np.array(caps_freq))
+caps_raw, caps_freq=eg.capsfinder('/home/ps22344/Downloads/craig_0208', 0.5)
+listi.append(np.array(caps_freq))
 
-# single_raw, single_freq=eg.singleletterfinder('/home/ps22344/Downloads/craig_0208')
-# listi.append(np.array(single_freq))
+single_raw, single_freq=eg.singleletterfinder('/home/ps22344/Downloads/craig_0208')
+listi.append(np.array(single_freq))
 
-# clip_raw, clip_freq=eg.clippingcounter('/home/ps22344/Downloads/craig_0208')
-# listi.append(np.array(clip_freq))
+clip_raw, clip_freq=eg.clippingcounter('/home/ps22344/Downloads/craig_0208')
+listi.append(np.array(clip_freq))
 
-# acro_raw, acro_freq=eg.acronymcounter('/home/ps22344/Downloads/craig_0208')
-# listi.append(np.array(acro_freq))
+acro_raw, acro_freq=eg.acronymcounter('/home/ps22344/Downloads/craig_0208')
+listi.append(np.array(acro_freq))
+
+emos_raw, emos_freq=eg.emoticonfinder('/home/ps22344/Downloads/craig_0208')
+listi.append(np.array(emos_freq))
 
 # # for x in listi:
 	# # print "!!!!!\n\n", x
