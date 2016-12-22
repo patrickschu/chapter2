5e1e9e28dbfd9896be65a87830435df3a18249ad
U-laptop\ps <patrickschultz@utexas.edu>
2016-12-22 17:10:48 +0100

    this we run thru the tests

--- current/03_analysis_egrammar_1220.py
+++ current/03_analysis_egrammar_1220.py
@@ -1,43 +1,48 @@
 import egrammartools as eg
 import numpy as np
+import time
 
+completestart=time.time()
 listi=[]
 
-emos_raw, emos_freq=eg.emoticonfinder('/home/ps22344/Downloads/craigbalanced_0601')
-listi.append(emos_freq)
+emos_raw, emos_freq=eg.emoticonfinder('/home/ps22344/Downloads/craig_0208')
+listi.append(np.array(emos_freq))
 
-rep_raw, rep_freq= eg.repeatedpunctuationfinder('/home/ps22344/Downloads/craigbalanced_0601')
-listi.append(rep_freq)
-
-
-leet_raw, leet_freq= eg.leetcounter('/home/ps22344/Downloads/craigbalanced_0601')
-listi.append(leet_freq)
+rep_raw, rep_freq= eg.repeatedpunctuationfinder('/home/ps22344/Downloads/craig_0208')
+listi.append(np.array(rep_freq))
 
 
-rebfor_raw, rebfor_freq= eg.rebusfinder_for('/home/ps22344/Downloads/craigbalanced_0601')
-listi.append(rebfor_freq)
+# leet_raw, leet_freq= eg.leetcounter('/home/ps22344/Downloads/craig_0208')
+# listi.append(leet_freq)
 
-rebto_raw, rebto_freq= eg.rebusfinder_to('/home/ps22344/Downloads/craigbalanced_0601')
-listi.append(rebto_freq)
 
-rebtoo_raw, rebtoo_freq= eg.rebusfinder_too('/home/ps22344/Downloads/craigbalanced_0601')
-listi.append(rebtoo_freq)
+# rebfor_raw, rebfor_freq= eg.rebusfinder_for('/home/ps22344/Downloads/craig_0208')
+# listi.append(rebfor_freq)
 
-caps_raw, caps_freq=eg.capsfinder('/home/ps22344/Downloads/craigbalanced_0601', 0.5)
-listi.append(caps_freq)
+# rebto_raw, rebto_freq= eg.rebusfinder_to('/home/ps22344/Downloads/craig_0208')
+# listi.append(rebto_freq)
 
-single_raw, single_freq=eg.singleletterfinder('/home/ps22344/Downloads/craigbalanced_0601')
-listi.append(single_freq)
+# rebtoo_raw, rebtoo_freq= eg.rebusfinder_too('/home/ps22344/Downloads/craig_0208')
+# listi.append(rebtoo_freq)
 
-clip_raw, clip_freq=eg.clippingcounter('/home/ps22344/Downloads/craigbalanced_0601')
-listi.append(clip_freq)
+# caps_raw, caps_freq=eg.capsfinder('/home/ps22344/Downloads/craig_0208', 0.5)
+# listi.append(caps_freq)
 
-acro_raw, acro_freq=eg.acronymcounter('/home/ps22344/Downloads/craigbalanced_0601')
-listi.append(acro_freq)
+# single_raw, single_freq=eg.singleletterfinder('/home/ps22344/Downloads/craig_0208')
+# listi.append(single_freq)
 
-# for x in listi:
-	# print "!!!!!\n\n", x
+# clip_raw, clip_freq=eg.clippingcounter('/home/ps22344/Downloads/craig_0208')
+# listi.append(clip_freq)
+
+# acro_raw, acro_freq=eg.acronymcounter('/home/ps22344/Downloads/craig_0208')
+# listi.append(acro_freq)
+
+# # for x in listi:
+	# # print "!!!!!\n\n", x
 	
 t=np.column_stack(listi)
 
-#print t.shape
+print type(t), t.shape
+completeend=time.time()
+
+print "This took us {} minutes. So slow!".format((completestart=completeend)/60)
