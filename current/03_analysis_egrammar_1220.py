e3558b88a46c5133de15526ce042ef64e21c0f13
U-laptop\ps <patrickschultz@utexas.edu>
2016-12-23 16:18:49 +0100

    we finish the categoryarryamaker

--- current/03_analysis_egrammar_1220.py
+++ current/03_analysis_egrammar_1220.py
@@ -1,10 +1,34 @@
 import egrammartools as eg
+import clustertools as ct
 import numpy as np
 import time
+import os
+import codecs
 
 completestart=time.time()
 listi=[]
 dir="/home/ps22344/Downloads/craigbalanced_0601"
+
+
+categories_dict, no_of_categories = ct.categorymachine(dir, "category1")
+
+def categoryarraymachine(input_dir, category_tag, cat_dict):
+	"""
+	The categoryarraymachine iterates over the input_dir and collects category info for all files.
+	It maps the categories to the numbers contained in cat_dict and returns a np.array with results.
+	"""
+	results=[]
+	for pati in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
+		print pati
+		for fili in [i for i in os.listdir(os.path.join(input_dir, pati)) if not i.startswith(".")]:
+			with codecs.open(os.path.join(input_dir, pati, fili), "r", "utf-8") as inputfili:
+				category=ct.tagextractor(inputfili.read(), category_tag, fili)
+			results.append([cat_dict[category]])
+			print "array", category, [cat_dict[category]]
+	return np.array(results)
+	
+category1=categoryarraymachine(dir, "category1", categories_dict)
+listi.append(category1)
 
 rep_raw, rep_freq= eg.repeatedpunctuationfinder(dir)
 listi.append(np.array(rep_freq))
@@ -45,5 +69,9 @@ t=np.column_stack(listi)
 
 print type(t), t.shape
 completeend=time.time()
+
+print t
+
+print categories_dict
 
 print "This took us {} minutes. So slow!".format((completeend-completestart)/60)
