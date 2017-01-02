6927f47c32624b0458f4fcd75fdf731986ea8e63
U-laptop\ps <patrickschultz@utexas.edu>
2017-01-02 12:38:19 +0100

    put feature collection in a func; clean up; this we upload

--- current/03_analysis_egrammar_1226.py
+++ current/03_analysis_egrammar_1226.py
@@ -3,108 +3,120 @@ import clustertools as ct
 import numpy as np
 import time
 import scipy
-from guppy import hpy
 
 headline="\n\n-----------\n\n"
 
+def featurecollector(categories, uniqs):
+	"""
+	collects those features, returns a np array with frequencies. 
+	first item in returned list is a category, 2nd a uniq number. 
+	"""
+	listi=[]
+	listi.append(("category1", category1))
+	listi.append(("uniqs", uniqs ))
+	##collect features
+	rep_raw, rep_freq= eg.repeatedpunctuationfinder(dir)
+	print "shape", rep_freq.shape
+	rep_freq=rep_freq.sum(axis=1)
+	print "shape", rep_freq.shape
+	print range(0, [int(1) if len(rep_freq.shape) < 2 else rep_freq.shape[1] for i in [1]][0])
+	listi.append((["repeated_punctuation"+str(count) for count in range(0, [int(1) if len(rep_freq.shape) < 2 else rep_freq.shape[1] for i in [1]][0])], rep_freq))
+
+
+	leet_raw, leet_freq= eg.leetcounter(dir)
+	print "shape", leet_freq.shape
+	leet_freq=leet_freq.sum(axis=1)
+	listi.append((["leetspeak"+str(count) for count in range(0, [int(1) if len(leet_freq.shape) < 2 else leet_freq.shape[1] for i in [1]][0])], leet_freq))
+
+
+
+	#all rebus go together
+	rebfor_raw, rebfor_freq= eg.rebusfinder_for(dir)
+	rebto_raw, rebto_freq= eg.rebusfinder_to(dir)
+	rebtoo_raw, rebtoo_freq= eg.rebusfinder_too(dir)
+	rebus_freq=rebtoo_freq+rebto_freq+rebfor_freq
+	listi.append((["rebus"+str(count) for count in range(0, [int(1) if len(rebus_freq.shape) < 2 else rebus_freq.shape[1] for i in [1]][0])], rebus_freq))
+	print "shape of rebus", np.array(rebus_freq).shape
+	print rebus_freq
+
+
+	caps_raw, caps_freq=eg.capsfinder(dir, 0.5)
+	print "shape", caps_freq.shape
+	caps_freq=caps_freq.sum(axis=1)
+	print caps_freq.shape
+	listi.append((["capitalization"+str(count) for count in range(0, [int(1) if len(caps_freq.shape) < 2 else caps_freq.shape[1] for i in [1]][0])], caps_freq))
+
+	single_raw, single_freq=eg.singleletterfinder(dir)
+	print "shape", single_freq.shape
+	single_freq=single_freq.sum(axis=1)
+	listi.append((["single_letters"+str(count) for count in range(0, [int(1) if len(single_freq.shape) < 2 else single_freq.shape[1] for i in [1]][0])], single_freq))
+
+	clip_raw, clip_freq=eg.clippingcounter(dir)
+	print "shape", clip_freq.shape
+	clip_freq=clip_freq.sum(axis=1)
+	listi.append((["clippings"+str(count) for count in range(0, [int(1) if len(clip_freq.shape) < 2 else clip_freq.shape[1] for i in [1]][0])], clip_freq))
+
+	acro_raw, acro_freq=eg.acronymcounter(dir)
+	print "shape", acro_freq.shape
+	acro_freq=acro_freq.sum(axis=1)
+	listi.append((["acronyms"+str(count) for count in range(0, [int(1) if len(acro_freq.shape) < 2 else acro_freq.shape[1] for i in [1]][0])], acro_freq))
+
+	emos_raw, emos_freq=eg.emoticonfinder(dir)
+	print "shape", emos_freq.shape
+	emos_freq=emos_freq.sum(axis=1)
+	listi.append((["emoticons"+str(count) for count in range(0, [int(1) if len(emos_freq.shape) < 2 else emos_freq.shape[1] for i in [1]][0])], emos_freq))
+
+
+	pros_raw, pros_freq=eg.prosodycounter(dir)
+	print "shape", pros_freq.shape
+	pros_freq=pros_freq.sum(axis=1)
+	listi.append((["prosody"+str(count) for count in range(0, [int(1) if len(pros_freq.shape) < 2 else pros_freq.shape[1] for i in [1]][0])], pros_freq))
+	return listi
 
 completestart=time.time()
-listi=[]
+
 dir="/home/ps22344/Downloads/craigbalanced_0601_small"
 
 ##prep
 #add cats
 categories_dict, no_of_categories = ct.categorymachine(dir, "category1")
 category1=ct.categoryarraymachine(dir, "category1", categories_dict)
-listi.append(("category1", category1))
-
 
 #add uniqs
 uniqs, file_count, filedicti=ct.uniqarraymachine(dir, 0) 	
 print "So many files", file_count
-listi.append(("uniqs", uniqs ))
 
-
-##collect features
-rep_raw, rep_freq= eg.repeatedpunctuationfinder(dir)
-print "shape", rep_freq.shape
-rep_freq=rep_freq.sum(axis=1)
-print "shape", rep_freq.shape
-print range(0, [int(1) if len(rep_freq.shape) < 2 else rep_freq.shape[1] for i in [1]][0])
-listi.append((["repeated_punctuation"+str(count) for count in range(0, [int(1) if len(rep_freq.shape) < 2 else rep_freq.shape[1] for i in [1]][0])], rep_freq))
-
-
-leet_raw, leet_freq= eg.leetcounter(dir)
-print "shape", leet_freq.shape
-leet_freq=leet_freq.sum(axis=1)
-listi.append((["leetspeak"+str(count) for count in range(0, [int(1) if len(leet_freq.shape) < 2 else leet_freq.shape[1] for i in [1]][0])], leet_freq))
-
-
-
-#all rebus go together
-rebfor_raw, rebfor_freq= eg.rebusfinder_for(dir)
-rebto_raw, rebto_freq= eg.rebusfinder_to(dir)
-rebtoo_raw, rebtoo_freq= eg.rebusfinder_too(dir)
-rebus_freq=rebtoo_freq+rebto_freq+rebfor_freq
-listi.append((["rebus"+str(count) for count in range(0, [int(1) if len(rebus_freq.shape) < 2 else rebus_freq.shape[1] for i in [1]][0])], rebus_freq))
-print "shape of rebus", np.array(rebus_freq).shape
-print rebus_freq
-
-
-caps_raw, caps_freq=eg.capsfinder(dir, 0.5)
-print "shape", caps_freq.shape
-caps_freq=caps_freq.sum(axis=1)
-print caps_freq.shape
-listi.append((["capitalization"+str(count) for count in range(0, [int(1) if len(caps_freq.shape) < 2 else caps_freq.shape[1] for i in [1]][0])], caps_freq))
-
-single_raw, single_freq=eg.singleletterfinder(dir)
-print "shape", single_freq.shape
-single_freq=single_freq.sum(axis=1)
-listi.append((["single_letters"+str(count) for count in range(0, [int(1) if len(single_freq.shape) < 2 else single_freq.shape[1] for i in [1]][0])], single_freq))
-
-clip_raw, clip_freq=eg.clippingcounter(dir)
-print "shape", clip_freq.shape
-clip_freq=clip_freq.sum(axis=1)
-listi.append((["clippings"+str(count) for count in range(0, [int(1) if len(clip_freq.shape) < 2 else clip_freq.shape[1] for i in [1]][0])], clip_freq))
-
-acro_raw, acro_freq=eg.acronymcounter(dir)
-print "shape", acro_freq.shape
-acro_freq=acro_freq.sum(axis=1)
-listi.append((["acronyms"+str(count) for count in range(0, [int(1) if len(acro_freq.shape) < 2 else acro_freq.shape[1] for i in [1]][0])], acro_freq))
-
-emos_raw, emos_freq=eg.emoticonfinder(dir)
-print "shape", emos_freq.shape
-emos_freq=emos_freq.sum(axis=1)
-listi.append((["emoticons"+str(count) for count in range(0, [int(1) if len(emos_freq.shape) < 2 else emos_freq.shape[1] for i in [1]][0])], emos_freq))
-
-
-pros_raw, pros_freq=eg.prosodycounter(dir)
-print "shape", pros_freq.shape
-pros_freq=pros_freq.sum(axis=1)
-listi.append((["prosody"+str(count) for count in range(0, [int(1) if len(pros_freq.shape) < 2 else pros_freq.shape[1] for i in [1]][0])], pros_freq))
 
 #put into one matrix
+listi=featurecollector(category1, uniqs)
 t=np.column_stack([i[1] for i in listi])
+print "original matrix",  type(t), t.shape
 
-
-print type(t), t.shape
-completeend=time.time()
-
-#this is the t w/out category
-#it is without category and without unique as in the matrixmachine
-wordmatrix_without_cat=t[:,2:]
-print "matrix w/out cat and uniq", wordmatrix_without_cat.shape
-print "\n\n\n++++", wordmatrix_without_cat, headline
-#this one keeps the category and the uniq
-wordmatrix_with_cat=t
-print "matrix with cat and uniq", wordmatrix_with_cat.shape
-#zscored matrix
-#wordmatrix_without_cat=scipy.stats.zscore(t[:,2:], axis=0)
-ct.matrixstats(wordmatrix_without_cat, wordmatrix_with_cat)
+##
+###category and feature dicts
 catdicti=categories_dict
 featuredict=[i[0] for i in listi if i[0] not in ["uniqs", "category1"]]
 #flatten it
 featuredict=[n for i in featuredict for n in i]
+
+##
+###data matrices
+#this is the t w/out category without category and without unique as in the matrixmachine
+wordmatrix_without_cat=t[:,2:]
+#this one keeps the category and the uniq
+wordmatrix_with_cat=t
+ct.meanmachine(wordmatrix_with_cat, categories_dict, featuredict, 100)
+
+##ZSCORES?
+#zscored matrix
+wordmatrix_without_cat=scipy.stats.zscore(t[:,2:], axis=0)
+wordmatrix_with_cat=np.column_stack([category1, uniqs, scipy.stats.zscore(t[:,2:], axis=0)])
+print "ayayay", wordmatrix_with_cat
+
+
+print "matrix with cat and uniq", wordmatrix_with_cat.shape
+print "matrix w/out cat and uniq", wordmatrix_without_cat.shape
+ct.matrixstats(wordmatrix_without_cat, wordmatrix_with_cat)
 
 
 print "feature dict", featuredict, len(featuredict)
@@ -112,8 +124,8 @@ print "feature dict", featuredict, len(featuredict)
 ##ADD SPELLING!!!!TO DO
 print wordmatrix_with_cat
 print wordmatrix_with_cat.shape
-ct.meanmachine(wordmatrix_with_cat, categories_dict, featuredict, 4)
 
+completeend=time.time()
 
 def main(distance_metric, testmode=False):
 	starttime=time.time()
@@ -196,9 +208,7 @@ def main(distance_metric, testmode=False):
 	print headline, "This took us {} minutes".format(process/60)
 
 
-#main('manhattan', testmode=False)
-
-
+main('manhattan', testmode=False)
 
 
 print "This took us {} minutes. So slow!".format((completeend-completestart)/60)
