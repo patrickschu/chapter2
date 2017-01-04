319814f558e204d0a615b71b77b956dadacd76ec
U-laptop\ps <patrickschultz@utexas.edu>
2017-01-04 18:42:31 +0100

    finalized for upload; json output added

--- current/getting_word_counts_0105.py
+++ current/getting_word_counts_0105.py
@@ -1,10 +1,9 @@
 import os
 import codecs
 import clustertools as ct
-
+import json
 
 dir = "/home/ps22344/Downloads/craigbalanced_0601_small"
-
 
 categorydict, catnumber = ct.categorymachine(dir, "category1")
 
@@ -24,8 +23,29 @@ def wordcounter(input_dir, category_tag, category_dict):
 	-------
 	something
 	"""
-	
-	
-	
+	print "Running the wordcounter"
+	resultdict=category_dict
+	for pati in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
+		print pati
+		for fili in [i for i in os.listdir(os.path.join(input_dir, pati)) if not i.startswith(".")]:
+			with codecs.open(os.path.join(input_dir, pati, fili), "r", "utf-8") as inputfili:
+				inputfili= inputfili.read()
+			wordcount= len(ct.tokenizer(ct.adtextextractor(inputfili, fili), remove_punctuation=True))
+			print wordcount
+			category= ct.tagextractor(inputfili, category_tag, fili)
+			print category
+			print resultdict.get(category)
+			if category in resultdict:
+				resultdict[category]= resultdict[category]+wordcount
+				print resultdict
+			else:
+				print "\n\nWARNING:\n{} is not in the category_dict. What do we do now?\n\n".format(category)
+	print "Wordcounter done"
+	print resultdict
+	with codecs.open("wordcounter_"+category_tag+".json", "w", "utf-8") as jsonout:
+		json.dump(resultdict, jsonout)
+
+		
+wordcounter(dir, "category1", categorydict)	
 	
 	
