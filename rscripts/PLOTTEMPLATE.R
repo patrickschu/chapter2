0a27366afc10c4c79e1a498a1a58398fbd04f152
U-laptop\ps <patrickschultz@utexas.edu>
2017-01-04 15:59:29 +0100

    add ordering for factors

--- rscripts/PLOTTEMPLATE.R
+++ rscripts/PLOTTEMPLATE.R
@@ -5,3 +5,6 @@
 #output PNGs on ltr size
 png(paste(filename+".png"), width=279.4, height=215.9, unit="mm", res=500)
 
+#ordering features and categories
+data_set[["Category"]]=ordered(data_set[["Category"]], levels=c("m4m", "m4w", "w4w", "w4m"))
+data_set[["Feature"]]=ordered(data_set[["Feature"]], levels=sort(levels(data_set[["Feature"]]), decreasing=F))
