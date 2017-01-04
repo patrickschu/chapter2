519462cfd720aa083125d630f36e755cf84c1779
U-laptop\ps <patrickschultz@utexas.edu>
2017-01-04 19:16:56 +0100

    finalize, rm printouts

--- current/getting_word_counts_0105.py
+++ current/getting_word_counts_0105.py
@@ -3,7 +3,7 @@ import codecs
 import clustertools as ct
 import json
 
-dir = "/home/ps22344/Downloads/craigbalanced_0601_small"
+dir = "/home/ps22344/Downloads/craig_0208"
 
 categorydict, catnumber = ct.categorymachine(dir, "category1")
 
@@ -31,17 +31,13 @@ def wordcounter(input_dir, category_tag, category_dict):
 			with codecs.open(os.path.join(input_dir, pati, fili), "r", "utf-8") as inputfili:
 				inputfili= inputfili.read()
 			wordcount= len(ct.tokenizer(ct.adtextextractor(inputfili, fili), remove_punctuation=True))
-			print wordcount
 			category= ct.tagextractor(inputfili, category_tag, fili)
-			print category
-			print resultdict.get(category)
 			if category in resultdict:
 				resultdict[category]= resultdict[category]+wordcount
-				print resultdict
 			else:
 				print "\n\nWARNING:\n{} is not in the category_dict. What do we do now?\n\n".format(category)
 	print "Wordcounter done"
-	print resultdict
+
 	with codecs.open("wordcounter_"+category_tag+".json", "w", "utf-8") as jsonout:
 		json.dump(resultdict, jsonout)
 
