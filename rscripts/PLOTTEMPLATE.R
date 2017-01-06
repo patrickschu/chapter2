d821707c9d79a736f8ea6347a144b81d6dfb41c8
U-laptop\ps <patrickschultz@utexas.edu>
2017-01-07 00:20:21 +0100

    add sort command to the general template

--- rscripts/PLOTTEMPLATE.R
+++ rscripts/PLOTTEMPLATE.R
@@ -8,3 +8,4 @@ png(paste(filename+".png"), width=279.4, height=215.9, unit="mm", res=500)
 #ordering features and categories
 data_set[["Category"]]=ordered(data_set[["Category"]], levels=c("m4m", "m4w", "w4w", "w4m"))
 data_set[["Feature"]]=ordered(data_set[["Feature"]], levels=sort(levels(data_set[["Feature"]]), decreasing=F))
+data_set=data_set[order(data_set[["feature"]], decreasing=F),]
