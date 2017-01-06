d24e407b3ec4ca1eedb9bdeb2f8b9399e3da46df
U-laptop\ps <patrickschultz@utexas.edu>
2017-01-07 00:21:00 +0100

    adapt scale to new data, fix font sizes

--- rscripts/plot_egrammar_by_category_0102.R
+++ rscripts/plot_egrammar_by_category_0102.R
@@ -4,24 +4,19 @@ setwd('E:/cygwin/home/ps22344/Downloads/chapter2/spreadsheets/')
 
 ##READ IN
 tt=read.csv(
-'E:/cygwin/home/ps22344/Downloads/chapter2/spreadsheets/egrammarstats_only4_0106.csv', 
+'E:/cygwin/home/ps22344/Downloads/chapter2/spreadsheets/egrammarstats_only4_0106_meansadded_ranksadded.csv', 
 header=T, 
 fileEncoding="UTF-8")
 
-overall=read.csv(
-'E:/cygwin/home/ps22344/Downloads/chapter2/spreadsheets/wordcounter_categories_only4_0106.csv', 
-header=T,
-fileEncoding="UTF-8",
-sep="\t")
+
 
 
 
 
 ##COMPUTE MEANS AND RANKS
-tt=cbind(tt, overall)
-tt[["mean"]]= (tt[["sum"]])/(tt[["Wordcount"]])
-write.csv(tt, "outi.csv", fileEncoding="UTF-8")
-#tt[["rank"]]= order(tt[["mean"]], decreasing=TRUE)
+#tt=cbind(tt, overall)
+#tt[["mean"]]= (tt[["sum"]])/(tt[["Wordcount"]])
+#write.csv(tt, "outi.csv", fileEncoding="UTF-8")
 
 #FORMAT aka C&P
 tt[["category"]]=ordered(tt[["category"]], levels=c("m4m", "m4w", "w4w", "w4m"))
@@ -31,9 +26,10 @@ rankplotter = function(data_set, category_column, rank_column)
 #this plots a barplot per category (as listed in category_column
 #with the category's rank (from rank_column) in each feature
 {
+data_set=data_set[order(data_set[["feature"]], decreasing=F),]
 print (summary(data_set));
 print (colnames(data_set));
-print (class(data_set[["N"]]));
+print (data_set[["feature"]]);
 #print (levels(factor(data_set[[rank_column]])));
 #this is to make plotting easier: plot a 4 for # 1 ranked
 data_set[["plotranks"]]= 5 - data_set[[rank_column]]
@@ -45,7 +41,7 @@ par(mfrow=c(2,2))
 for (c in levels(factor(data_set[[category_column]])))
 	{
 	cat ("cati", c, "\n");
-	print (data_set[data_set[[category_column]] == c, ][["Rank"]]);
+	print (data_set[data_set[[category_column]] == c, ][["rank"]]);
 	print (data_set[data_set[[category_column]] == c, ][["plotranks"]]);
 	bari=barplot(
 	data_set[data_set[[category_column]] == c, ][["plotranks"]],
@@ -53,12 +49,12 @@ for (c in levels(factor(data_set[[category_column]])))
 	ylim=c(0,4),
 	axes=FALSE);
 	axis(2, at=1:4, labels= rev(c("1st", "2nd", "3rd", "4th")));
-	text(bari, 0.5,
-	c("Leetspeak", "Rebus", "Capitals", "Single Letter", "Clippings", "Acronyms", "Emoticons", "Prosody"),
-	 cex=0.6, pos=3)
+	#read: position @.25, to the top of it (pos), srt for vertical
+	text(bari-0.25,  y=0.25, pos=4, c("Acronyms", "Capitals", "Clippings", "Emoticons", "Leetspeak", "Prosody", "Rebus", "Punctuation", "Single Letters"),
+	cex=1,  srt=90)
 	}
 dev.off()
 }
 
 
-rankplotter(tt[tt[["N"]]>1000,], "Category", "Rank")
+rankplotter(tt[tt[["N"]]>1000,], "category", "rank")
