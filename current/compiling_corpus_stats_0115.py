189f89ffa4aac74ee2c4e299b537676200a71510
patrick <patrickschultz@utexas.edu>
2017-01-16 13:10:31 -0600

    improve for flezible tag extraction

--- current/compiling_corpus_stats_0115.py
+++ current/compiling_corpus_stats_0115.py
@@ -1,29 +1,52 @@
 import os
+import codecs
 import clustertools as ct
 from collections import defaultdict
+import time
+import json
+
+timestamp = time.strftime("%Y%m%d-%H%M%S")
 
 """Compiling corpus stats"""
 
 input_dir="/Users/ps22344/Downloads/craig_0208"
 
 
-def wordcounter(input_dir):
+def wordcounter(input_dir, tag, output_json=False):
 	"""
 	count words 
 	"""
+	catdicti=defaultdict(float)
+	worddicti=defaultdict(float)
 	for dir in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
-		catdicti=defaultdict(float)
-		worddicti=defaultdict(float)
 		print dir
 		for fili in [i for i in os.listdir(os.path.join(input_dir, dir)) if not i.startswith(".")]:
 			inputad=ct.Ad(os.path.join(input_dir, dir, fili))
-			catdicti[inputad.meta['category1']]=catdicti[inputad.meta['category1']]+1
-			worddicti[inputad.meta['category1']]=worddicti[inputad.meta['category1']]+inputad.wordcount
-	print worddicti
-			
+			catdicti[inputad.meta[tag]]=catdicti[inputad.meta[tag]]+1
+			worddicti[inputad.meta[tag]]=worddicti[inputad.meta[tag]]+inputad.wordcount
+	if output_json:
+		with codecs.open(tag+"_categorydict"+timestamp+".txt", "w", "utf-8") as categoryout:
+			json.dump(catdicti, categoryout)
+		with codecs.open(tag+"_wordcountdict"+timestamp+".txt", "w", "utf-8") as wordout:
+			json.dump(worddicti, wordout)
+	for dict in [worddicti, catdicti]:
+		sorteddict=sorted(dict, key=lambda x:dict[x], reverse=True)
+		print "\n".join([",".join([i, unicode(dict[i])]) for i in sorteddict])
 	
 			
 			
-			
 		
-wordcounter(input_dir)
+wordcounter(input_dir, "gender", output_json=True)
+
+
+def dictprinter(file_name):
+	"""
+	put in json, get out csv print
+	"""
+	with codecs.open(file_name, "r", "utf-8") as inputjson:
+		dict=json.load(inputjson)
+	sorteddict=sorted(dict, key=lambda x:dict[x], reverse=True)
+	print "\n".join([",".join([i, unicode(dict[i])]) for i in sorteddict])
+	print "\ntotal count", sum([v for k,v in dict.items()])
+	
+#dictprinter('categorydict20170116-124503.txt')
