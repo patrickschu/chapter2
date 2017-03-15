c1f7d491b88bbc0f87c1dcc58f1a47240cf81528
patrick <patrickschultz@utexas.edu>
2017-03-14 19:35:11 -0500

    mpve

--- old_scripts/02_basic_stats_0210.R
+++
@@ -1,9 +0,0 @@
-#craig=read.csv(file.choose(), header=T)
-#summary(craig)
-
-#write.csv(xtabs(~craig$gender))
-
-t=split(craig,craig$type)
-#write.csv(xtabs(~t$ad$gender))
-
-write.csv(tapply(t$ad$wordcount, t$ad$gender, sum))
